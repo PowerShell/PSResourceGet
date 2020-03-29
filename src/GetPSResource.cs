e7945f5c30ad4758badfe790fa8a9fe8af77b584
Amber Erickson <americks@microsoft.com>
2020-03-28 21:28:26 -0700

    Update functionality for getting installed psresources -- prelease param not implemented

--- src/GetPSResource.cs
+++ src/GetPSResource.cs
@@ -1,20 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-//using System;
-//using System.Management.Automation;
-//using Microsoft.PowerShell.Commands;
-//using Microsoft.PowerShell.PowerShellGet.RepositorySettings;
-
-
 using NuGet.Versioning;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Management.Automation;
-
-
+using static System.Environment;
+using MoreLinq;
 
 namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
 {
@@ -59,7 +53,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
         }
         private string _version;
 
-
+        /*
         /// <summary>
         /// Specifies to include prerelease versions
         /// </summary>
@@ -73,8 +67,12 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
             { _prerelease = value; }
         }
         private SwitchParameter _prerelease;
+        */
 
-
+        public static readonly string OsPlatform = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+      
+        private string programFilesPath;
+        private string myDocumentsPath;
 
         /// <summary>
         /// </summary>
@@ -82,107 +80,291 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
         {
             // list all installation locations:
             // TODO: update paths
-            var psModulePath = "C:\\code\\temp\\sinstalltestpath";
+            string psModulePath = "";
+
+            var isWindows = OsPlatform.ToLower().Contains("windows");
+
+            // if not core
+            var isWindowsPS = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory().ToLower().Contains("windows") ? true : false;
+            
+            if (isWindowsPS)
+            {
+                programFilesPath = Path.Combine(Environment.GetFolderPath(SpecialFolder.ProgramFiles), "WindowsPowerShell");
+                /// TODO:  Come back to this
+                var userENVpath = Path.Combine(Environment.GetEnvironmentVariable("USERPROFILE"), "Documents");
+
+
+                myDocumentsPath = Path.Combine(Environment.GetFolderPath(SpecialFolder.MyDocuments), "WindowsPowerShell");
+            }
+            else
+            {
+                programFilesPath = Path.Combine(Environment.GetFolderPath(SpecialFolder.ProgramFiles), "PowerShell");
+                myDocumentsPath = Path.Combine(Environment.GetFolderPath(SpecialFolder.MyDocuments), "PowerShell");
+            }
+
 
 
 
             // 1) Create a list of either
             // Of all names
-            var versionDirs = Directory.GetDirectories(psModulePath).ToList();
+
+            var dirsToSearch = new List<string>();
+            try
+            {
+                
+                dirsToSearch.AddRange(Directory.GetDirectories(psModulePath).ToList());
+            }
+            catch { }
+         
+            var pfModulesPath = Path.Combine(programFilesPath, "Modules");
+            if (Directory.Exists(pfModulesPath))
+            {
+                dirsToSearch.AddRange(Directory.GetDirectories(pfModulesPath).ToList());
+            }
+
+            var pfScriptsPath = Path.Combine(programFilesPath, "Scripts");
+            if (Directory.Exists(pfScriptsPath))
+            {
+                dirsToSearch.AddRange(Directory.GetDirectories(pfScriptsPath).ToList());
+            }
+            
+           
+            var mdModulesPath = Path.Combine(myDocumentsPath, "Modules");  // change programfiles to mydocuments
+            if (Directory.Exists(mdModulesPath))
+            {
+                dirsToSearch.AddRange(Directory.GetDirectories(mdModulesPath).ToList());
+            }
+
+            var mdScriptsPath = Path.Combine(myDocumentsPath, "Scripts"); // change programFiles to myDocuments
+            if (Directory.Exists(mdScriptsPath))
+            {
+                dirsToSearch.AddRange(Directory.GetDirectories(mdScriptsPath).ToList());
+            }
+       
+
 
             // Or a list of the passed in names
-            if (_name != null)
+            if (_name != null && !_name[0].Equals("*"))
             {
-                versionDirs = versionDirs.FindAll(p => _name.Contains(p));
+                var nameLowerCased = new List<string>();
+                Array.ForEach(_name, n => nameLowerCased.Add(n.ToLower()));
+                dirsToSearch = dirsToSearch.FindAll(p => nameLowerCased.Contains(new DirectoryInfo(p).Name.ToLower()));   //// test this out tomorrow 
             }
 
 
-
-            NuGetVersion nugetVersion;
-            var bleh = NuGetVersion.TryParse(_version, out nugetVersion);
-
+            // try to parse into a specific NuGet version
             VersionRange versionRange = null;
-            if (nugetVersion == null)
+            if (_version != null)
             {
-                VersionRange.TryParse(_version, out versionRange);
-            }
+                NuGetVersion specificVersion;
+                NuGetVersion.TryParse(_version, out specificVersion);
 
-
-
-            List<string> dirsToReturn = new List<string>();
-
-            // check whether we want to include prerelease versions or not
-            // If prereleaseOnly is not specified, we'll only take into account stable versions of pkgs
-            if (!_prerelease)
-            {
-                List<string> stableVersionDirs = new List<string>();
-                foreach (var dir in versionDirs)
+                if (specificVersion != null)
                 {
-                    var nameOfDir = Path.GetFileName(dir);
-                    var nugVersion = NuGetVersion.Parse(nameOfDir);
-
-                    if (nugVersion.IsPrerelease)
-                    {
-                        stableVersionDirs.Remove(dir);
-                    }
+                    // exact version
+                    versionRange = new VersionRange(specificVersion, true, specificVersion, true, null, null);
                 }
-                versionDirs = stableVersionDirs;
+                else
+                {
+                    // check if version range
+                    versionRange = VersionRange.Parse(_version);
+                }
             }
 
 
 
+
+            List<string> installedPkgsToReturn = new List<string>();
+
+
+
+            IEnumerable<string> returnPkgs = null;
+            var versionDirs = new List<string>();
+
+           
 
             //2) use above list to check 
             // if the version specificed is a version range
             if (versionRange != null)
             {
-
-                foreach (var versionDirPath in versionDirs)
+                foreach (var pkgPath in dirsToSearch)
                 {
-                    var nameOfDir = Path.GetFileName(versionDirPath);
-                    var nugVersion = NuGetVersion.Parse(nameOfDir);
 
-                    if (versionRange.Satisfies(nugVersion))
+                    var versionsDirs = Directory.GetDirectories(pkgPath);
+
+                    foreach (var versionPath in versionsDirs)
                     {
-                        dirsToReturn.Add(versionDirPath);
-                    }
-                }
-            }
-            else if (nugetVersion != null)
-            {
-                // if the version specified is a version
 
-                dirsToReturn.Add(nugetVersion.ToNormalizedString());
+                        NuGetVersion dirAsNugetVersion;
+                        var dirInfo = new DirectoryInfo(versionPath);
+                        NuGetVersion.TryParse(dirInfo.Name, out dirAsNugetVersion);
+
+                        if (versionRange.Satisfies(dirAsNugetVersion))
+                        {
+                            // just search scripts paths
+                            if (pkgPath.ToLower().Contains("scripts"))
+                            {
+                                // TODO check if scripts are installed
+                                var scriptXmls = Directory.GetFiles(pkgPath);
+                                if (_name == null || _name[0].Equals("*"))
+                                {
+                                    // Add all the script xmls
+                                    installedPkgsToReturn.AddRange(scriptXmls);
+                                }
+                                else
+                                {
+                                    // Just add the xmls of the names specified
+                                    foreach (var name in _name)
+                                    {
+                                        var scriptXMLPath = Path.Combine(pkgPath, name, "_InstalledScriptInfo");
+
+                                        if (File.Exists(scriptXMLPath))
+                                        {
+                                            installedPkgsToReturn.Add(scriptXMLPath);
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                // modules paths
+                                versionsDirs = Directory.GetDirectories(pkgPath);
+
+                                // Check if the pkg path actually has version sub directories.
+                                if (versionsDirs.Length != 0)
+                                {
+                                    Array.Sort(versionsDirs, StringComparer.OrdinalIgnoreCase);
+                                    Array.Reverse(versionsDirs);
+
+                                    var pkgXmlFilePath = Path.Combine(versionsDirs.First(), "PSGetModuleInfo.xml");
+
+                                    // TODO:  check if this xml file exists, if it doesn't check if it exists in a previous version
+
+                                    installedPkgsToReturn.Add(pkgXmlFilePath);
+                                }
+                            }
+
+
+
+                            installedPkgsToReturn.Add(versionPath);
+                        }
+                    }  
+                }
             }
             else
             {
-                // if no version specified (just the latest version)
+                // THIS SHOULD BE DONE
                 // if no version is specified, just get the latest version
-                versionDirs.Sort();
+                foreach (var pkgPath in dirsToSearch)
+                {
+                    // just search scripts paths
+                    if (pkgPath.ToLower().Contains("scripts"))
+                    {
+                        // TODO check if scripts are installed
+                        var scriptXmls = Directory.GetFiles(pkgPath);
+                        if (_name == null || _name[0].Equals("*"))
+                        {
+                            // Add all the script xmls
+                            installedPkgsToReturn.AddRange(scriptXmls);
+                        }
+                        else
+                        {
+                            // Just add the xmls of the names specified
+                            foreach (var name in _name)
+                            {
+                                var scriptXMLPath = Path.Combine(pkgPath, name, "_InstalledScriptInfo");
 
-                dirsToReturn.Add(versionDirs.First());
+                                if (File.Exists(scriptXMLPath))
+                                {
+                                    installedPkgsToReturn.Add(scriptXMLPath);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // modules paths
+                        string[] versionsDirs = new string[0];
+                       
+                        versionsDirs = Directory.GetDirectories(pkgPath);
+
+                        // Check if the pkg path actually has version sub directories.
+                        if (versionsDirs.Length != 0)
+                        {
+                            Array.Sort(versionsDirs, StringComparer.OrdinalIgnoreCase);
+                            Array.Reverse(versionsDirs);
+
+                            var pkgXmlFilePath = Path.Combine(versionsDirs.First(), "PSGetModuleInfo.xml");
+
+                            // TODO:  check if this xml file exists, if it doesn't check if it exists in a previous version
+
+                            installedPkgsToReturn.Add(pkgXmlFilePath);
+                        }
+                    }
+
+
+
+                  
+                }
             }
 
 
+            // Flatten returned pkgs before displaying output returnedPkgsFound.Flatten().ToList()[0]
+            var flattenedPkgs = installedPkgsToReturn.Flatten();
 
-            /// list all the modules, or the specific name / version
-            /// output:  version  |    name     |   repository      |   description
-
-
-
-            /*
-
-            foreach (var repo in listOfRepositories)
+            foreach (string xmlFilePath in flattenedPkgs)
             {
-                var repoPSObj = new PSObject();
-                repoPSObj.Members.Add(new PSNoteProperty("Name", repo.Name));
-                repoPSObj.Members.Add(new PSNoteProperty("Url", repo.Url));
-                repoPSObj.Members.Add(new PSNoteProperty("Trusted", repo.Trusted));
-                repoPSObj.Members.Add(new PSNoteProperty("Priority", repo.Priority));
-                WriteObject(repoPSObj);
-            }
+                // Open xml and read metadata from it     
+                if (File.Exists(xmlFilePath))
+                {
+                    ReadOnlyPSMemberInfoCollection<PSPropertyInfo> nameInfo;
+                    ReadOnlyPSMemberInfoCollection<PSPropertyInfo> versionInfo;
+                    ReadOnlyPSMemberInfoCollection<PSPropertyInfo> additionalMetadataInfo;
+                    ReadOnlyPSMemberInfoCollection<PSPropertyInfo> psDataInfo;
+                    ReadOnlyPSMemberInfoCollection<PSPropertyInfo> repositoryInfo;
+                    ReadOnlyPSMemberInfoCollection<PSPropertyInfo> descriptionversionInfo;
 
-    */
+                    var isPrelease = false;
+                    using (StreamReader sr = new StreamReader(xmlFilePath))
+                    {
+
+                        string text = sr.ReadToEnd();
+                        var deserializedObj = (PSObject)PSSerializer.Deserialize(text);
+
+                        nameInfo = deserializedObj.Properties.Match("Name");
+
+                        /* // testing adding prerelease parameter
+                        additionalMetadataInfo = deserializedObj.Properties.Match("AdditionalMetadata");
+                        if (additionalMetadataInfo.Any())
+                        {
+                            isPrelease = additionalMetadataInfo.FirstOrDefault().Value.ToString().Contains("IsPrerelease=true");
+                            if ((isPrelease == true) && _prerelease) // find a stable version of the pkg {}
+                        }
+                        */
+                       
+                        versionInfo = deserializedObj.Properties.Match("Version");
+                        repositoryInfo = deserializedObj.Properties.Match("Repository");
+                        descriptionversionInfo = deserializedObj.Properties.Match("Description");
+
+                    };
+
+                    // if -Prerelease is not passed in as a parameter, don't allow prerelease pkgs to be returned,
+                    // we still want all pkgs to be returned if -Prerelease is passed in as a param
+                    //if ((_prerelease == false && isPrelease == false) || _prerelease == true)
+                    //{
+                    PSObject pkgAsPSObject = new PSObject();
+                    try
+                    {
+                        pkgAsPSObject.Members.Add(new PSNoteProperty("Name", nameInfo.FirstOrDefault().Value));   // need to fix output
+                        pkgAsPSObject.Members.Add(new PSNoteProperty("Version", versionInfo.FirstOrDefault().Value));
+                        pkgAsPSObject.Members.Add(new PSNoteProperty("Repository", repositoryInfo.FirstOrDefault().Value));
+                        pkgAsPSObject.Members.Add(new PSNoteProperty("Description", descriptionversionInfo.FirstOrDefault().Value));
+                        WriteObject(pkgAsPSObject);
+                    }
+                    catch { }
+                    //}
+                }
+            }
         }
 
 
