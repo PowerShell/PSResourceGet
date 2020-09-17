7c9832b9d63fea3d9b18dea8ea1c6f2e8099eed0
Anam Navied <anam.naviyou@gmail.com>
2020-09-17 01:06:31 -0400

    changed file path generation to use Join-Path for cross-platform use

--- test/PSGetTestUtils.psm1
+++ test/PSGetTestUtils.psm1
@@ -6,24 +6,11 @@
 
 #."$PSScriptRoot\uiproxy.ps1"
 
-# import the .dll:
-$baseP = $PSScriptRoot -split "\\" #PSScriptRoot should be : C:\Users\annavied\Documents\PowerShellGet\test
-$basePWithoutTest = $baseP[0..($baseP.Length-2)] #basePWithoutTest should be: C:\Users\annavied\Documents\PowerShellGet
-$basePWithoutTest = $basePWithoutTest -Join "\"
-$script:fullPath = Join-Path $basePWithoutTest -ChildPath "\src\bin\Debug\netstandard2.0\publish\PowerShellGet.dll"
 
-Write-Host $script:fullPath
-Import-Module $script:fullPath
+$baseParentPath = Split-Path -Path $PSScriptRoot # removes test directory and returns remaning parent path
+$fullPath = Join-Path -Path $baseParentPath -ChildPath "src" -AdditionalChildPath ("bin", "Debug", "netstandard2.0", "publish", "PowerShellGet.dll")
+Import-Module $fullPath -Force
 
-# import the .psd1:
-# $baseP = $PSScriptRoot -split "\\"
-# $basePWithoutTest = $baseP[0..($baseP.Length-2)]
-# $basePWithoutTest = $basePWithoutTest -Join "\"
-# $script:fullPath = Join-Path $basePWithoutTest -ChildPath "\PowerShellGet.psd1" 
-# Write-Host "The full path is:"
-# Write-Host $script:fullPath
-# Write-Host "done writing it out"
-# Import-Module $script:fullPath -force
 
 $script:DotnetCommandPath = @()
 $script:EnvironmentVariableTarget = @{ Process = 0; User = 1; Machine = 2 }
