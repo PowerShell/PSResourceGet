2b1c918ac6b0c884fe3ba949e5001198f6d2f386
Justin Grote <justingrote@github-viewmysite.com>
2021-01-12 23:31:36 -0800

    Fix: PublishResource Tests

--- test/PublishResource.Tests.ps1
+++ test/PublishResource.Tests.ps1
@@ -2,81 +2,64 @@
 #
 # Copyright (c) Microsoft Corporation, 2019
 
-write-warning 'Publish-Resource Tests are temporarily disabled'
-return
-
 #Testing Environment Setup
 BeforeAll {
     Import-Module $PSScriptRoot/Shared.psm1
-}
 
-
-BeforeAll {
-    $psGetMod = Get-Module -Name PowerShellGet
-    if ((! $psGetMod) -or (($psGetMod | Select-Object Version) -lt 3.0.0))
-    {
-        Write-Verbose -Verbose "Importing PowerShellGet 3.0.0 for test"
-        Import-Module -Name PowerShellGet -MinimumVersion 3.0.0 -Force
-    }
-    
     $PSGalleryName = 'PSGallery'
     $PSGalleryLocation = 'https://www.powershellgallery.com/api/v2'
-    
     $TestLocalDirectory = 'TestLocalDirectory'
-    $tmpdir = Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath $TestLocalDirectory
+    $tmpdir = Join-Path -Path $TestDrive -ChildPath $TestLocalDirectory
     
     if (-not (Test-Path -LiteralPath $tmpdir)) {
         New-Item -Path $tmpdir -ItemType Directory > $null
     }
 }
 
-##########################
-### Publish-PSResource ###
-##########################
-Describe 'Test Publish-PSResource' -tags 'BVT' {
-
+Describe 'Publish-PSResource' -tags 'BVT' {
     BeforeAll {
-
-        Register-PSResourceRepository -Name psgettestlocal -URL "c:\code\testdir"
+        try {
+            Unregister-PSResourceRepository -Name psgettestlocal
+        } catch {}
+        Register-PSResourceRepository -Name psgettestlocal -URL $(Join-Path $testdrive 'psgettestlocal')
 
         # Create temp module to be published
         $script:TempModulesPath = Join-Path -Path $tmpdir -ChildPath "PSGet_$(Get-Random)"
-        $null = New-Item -Path $script:TempModulesPath -ItemType Directory -Force
+        $null = New-Item -Path $TempModulesPath -ItemType Directory -Force
 
-        $script:PublishModuleName = "TestPublishModule"
-        $script:PublishModuleBase = Join-Path $script:TempModulesPath $script:PublishModuleName
-        $null = New-Item -Path $script:PublishModuleBase -ItemType Directory -Force
-
+        $PublishModuleName = "TestPublishModule"
+        $PublishModuleBase = Join-Path $TempModulesPath $PublishModuleName
+        $null = New-Item -Path $PublishModuleBase -ItemType Directory -Force
     }
 	AfterAll { 
-        if($tempdir -and (Test-Path $tempdir))
+        if($tmpdir -and (Test-Path $tmpdir))
         {
-            Remove-Item $tempdir -Force -Recurse -ErrorAction SilentlyContinue
+            Remove-Item $tmpdir -Force -Recurse
         }
     }
 
     ### Publish a script
-    It 'Should publish a script' { Set-ItResult -Pending
+    It 'Should publish a script' { Set-ItResult -Pending -Because 'WIP'
 
         $Name = 'TestScriptName'
-        $scriptFilePath = Join-Path -Path $script:TempModulesPath -ChildPath "$Name.ps1"
+        $scriptFilePath = Join-Path -Path $TempModulesPath -ChildPath "$Name.ps1"
         $null = New-Item -Path $scriptFilePath -ItemType File -Force
 
         $version = "1.0.0"
         $params = @{
-                    #Path = $scriptFilePath
-                    Version = $version
-                    #GUID = 
-                    Author = 'Jane'
-                    CompanyName = 'Microsoft Corporation'
-                    Copyright = '(c) 2020 Microsoft Corporation. All rights reserved.'
-                    Description = "Description for the $Name script"
-                    LicenseUri = "https://$Name.com/license"
-                    IconUri = "https://$Name.com/icon"
-                    ProjectUri = "https://$Name.com"
-                    Tags = @('Tag1','Tag2', "Tag-$Name-$version")
-                    ReleaseNotes = "$Name release notes"
-                    }
+            #Path = $scriptFilePath
+            Version = $version
+            #GUID = 
+            Author = 'Jane'
+            CompanyName = 'Microsoft Corporation'
+            Copyright = '(c) 2020 Microsoft Corporation. All rights reserved.'
+            Description = "Description for the $Name script"
+            LicenseUri = "https://$Name.com/license"
+            IconUri = "https://$Name.com/icon"
+            ProjectUri = "https://$Name.com"
+            Tags = @('Tag1','Tag2', "Tag-$Name-$version")
+            ReleaseNotes = "$Name release notes"
+        }
 
         $scriptMetadata = New-PSScriptMetadata @params
         Set-Content -Path $scriptFilePath -Value $scriptMetadata
@@ -85,14 +68,13 @@ Describe 'Test Publish-PSResource' -tags 'BVT' {
     }
 
 
-    It 'Should publish a module'
-    {
-        $PublishModuleBase = Join-Path $script:TempModulesPath $script:PublishModuleName
+    It 'Should publish a module' { Set-ItResult -Pending -Because 'WIP'
+        $PublishModuleBase = Join-Path $TempModulesPath $PublishModuleName
 
         $version = "1.0"
-        New-ModuleManifest -Path (Join-Path -Path $script:PublishModuleBase -ChildPath "$script:PublishModuleName.psd1") -ModuleVersion $version -Description "$script:PublishModuleName module"  -NestedModules "$script:PublishModuleName.psm1"
+        New-ModuleManifest -Path (Join-Path -Path $PublishModuleBase -ChildPath "$PublishModuleName.psd1") -ModuleVersion $version -Description "$PublishModuleName module"  -NestedModules "$PublishModuleName.psm1"
 
-        Publish-PSResource -path  $script:PublishModuleBase -Repository psgettestlocal
+        Publish-PSResource -path $PublishModuleBase -Repository psgettestlocal
     }
 }
 
