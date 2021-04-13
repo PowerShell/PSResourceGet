079d596c03177e5e6bf5303e7481d99e3c4a69c5
Anam Navied <anam.naviyou@gmail.com>
2021-04-13 18:22:20 -0400

    added implemenation and test for Set

---
+++ src/code/SetPSResourceRepository.cs
@@ -0,0 +1,329 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections;
+using System.Management.Automation;
+using System.Globalization;
+using Microsoft.PowerShell.PowerShellGet.UtilClasses;
+using System.Collections.Generic;
+using Dbg = System.Diagnostics.Debug;
+
+namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
+{
+    /// <summary>
+    /// todo
+    /// </summary>
+    [Cmdlet(VerbsCommon.Set,
+        "PSResourceRepository",
+        DefaultParameterSetName = "NameParameterSet",
+        SupportsShouldProcess = true,
+        HelpUri = "<add>")]
+    public sealed
+    class SetPSResourceRepository : PSCmdlet
+    {
+        #region Members
+
+        private const string NameParameterSet = "NameParameterSet";
+        private const string RepositoriesParameterSet = "RepositoriesParameterSet";
+        private const int DefaultPriority = -1;
+        #endregion
+
+        #region Parameters
+
+        /// <summary>
+        /// Specifies the name of the repository to be set.
+        /// </sumamry>
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true, ParameterSetName = "NameParameterSet")]
+        [ArgumentCompleter(typeof(RepositoryNameCompleter))]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Specifies the location of the repository to be set.
+        /// </sumamry>
+        [Parameter(ValueFromPipelineByPropertyName = true, ParameterSetName = "NameParameterSet")]
+        [ValidateNotNullOrEmpty]
+        public Uri URL
+        {
+            get
+            { return _url; }
+
+            set
+            {
+                if (!Uri.TryCreate(value, string.Empty, out Uri url))
+                {
+                    var message = string.Format(CultureInfo.InvariantCulture, "The URL provided is not valid: {0}", value);
+                    var ex = new ArgumentException(message);
+                    var urlErrorRecord = new ErrorRecord(ex, "InvalidUrl", ErrorCategory.InvalidArgument, null);
+                    ThrowTerminatingError(urlErrorRecord);
+                }
+
+                _url = url;
+            }
+        }
+        private Uri _url = null;
+
+        /// <summary>
+        /// todo: also question: order params alphabetically? also why do we have this?
+        /// in Register we had ProxyCredential but that's it, also from pipeline ok?
+        /// </sumamry>
+        [Parameter(ValueFromPipelineByPropertyName = true, ParameterSetName = "NameParameterSet")]
+        public PSCredential Credential { get; set; } = null;
+
+        /// <summary>
+        /// Specifies a hashtable of repositories and is used to register multiple repositories at once.
+        /// </summary>
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = "RepositoriesParameterSet")]
+        [ValidateNotNullOrEmpty]
+        public Hashtable[] Repositories { get; set; }
+
+        /// <summary>
+        /// Specifies whether the repository should be trusted.
+        /// todo: why can't this be passed via pipeline?
+        /// </summary>
+        [Parameter(ParameterSetName = "NameParameterSet")]
+        public SwitchParameter Trusted
+        {
+            get
+            { return _trusted; }
+
+            set
+            {
+                _trusted = value;
+                isSet = true;
+            }
+        }
+        private SwitchParameter _trusted;
+        private bool isSet = false; // todo: just default initialize? also better implem?
+
+        /// <summary>
+        /// Specifies the priority ranking of the repository, such that repositories with higher ranking priority are searched
+        /// before a lower ranking priority one, when searching for a repository item across multiple registered repositories.
+        /// Valid priority values range from 0 to 50, such that a lower numeric value (i.e 10) corresponds
+        /// to a higher priority ranking than a higher numeric value (i.e 40). Has default value of 50.
+        /// </summary>
+        [Parameter(ValueFromPipelineByPropertyName = true, ParameterSetName = "NameParameterSet")]
+        [ValidateNotNullOrEmpty]
+        [ValidateRange(0, 50)]
+        public int Priority { get; set; } = DefaultPriority;
+
+        /// <summary>
+        /// Specifies a proxy server for the request, rather than a direct connection to the internet resource.
+        /// </summary>
+        [Parameter]
+        [ValidateNotNullOrEmpty]
+        public Uri Proxy { get; set; }
+
+        /// <summary>
+        /// Specifies a user account that has permission to use the proxy server that is specified by the Proxy parameter.
+        /// todo, why not also validate not null or empty? bc it's not a url and cred could be empty?
+        /// </summary>
+        [Parameter]
+        public PSCredential ProxyCredential { get; set; }
+
+        /// <summary>
+        /// When specified, displays the succcessfully registered repository and its information
+        /// </summary>
+        [Parameter]
+        public SwitchParameter PassThru { get; set; }
+
+        #endregion
+
+        #region Methods
+        protected override void BeginProcessing()
+        {
+            try
+            {
+                WriteDebug("Calling API to check repository store exists in non-corrupted state");
+                RepositorySettings.CheckRepositoryStore();
+            }
+            catch (PSInvalidOperationException e)
+            {
+                ThrowTerminatingError(new ErrorRecord(
+                    new PSInvalidOperationException(e.Message),
+                    "RepositoryStoreException",
+                    ErrorCategory.ReadError,
+                    this));
+            }
+        }
+
+        protected override void ProcessRecord()
+        {
+            if (Proxy != null || ProxyCredential != null)
+            {
+                ThrowTerminatingError(new ErrorRecord(
+                    new PSNotImplementedException("Proxy and ProxyCredential are not yet implemented. Please rerun cmdlet with other parameters."),
+                    "ParametersNotImplementedYet",
+                    ErrorCategory.NotImplemented,
+                    this));
+            }
+
+            List<PSRepositoryInfo> items = new List<PSRepositoryInfo>();
+
+            switch(ParameterSetName)
+            {
+                case NameParameterSet:
+                    try
+                    {
+                        items.Add(NameParameterSetHelper(Name, URL, Priority, _trusted));
+                    }
+                    catch (Exception e)
+                    {
+                        ThrowTerminatingError(new ErrorRecord(
+                            new PSInvalidOperationException(e.Message),
+                            "ErrorInNameParameterSet",
+                            ErrorCategory.InvalidArgument,
+                            this));
+                    }
+                    break;
+
+                case RepositoriesParameterSet:
+                    try
+                    {
+                        items = RepositoriesParameterSetHelper();
+                    }
+                    catch (Exception e)
+                    {
+                        ThrowTerminatingError(new ErrorRecord(
+                            new PSInvalidOperationException(e.Message),
+                            "ErrorInRepositoriesParameterSet",
+                            ErrorCategory.InvalidArgument,
+                            this));
+                    }
+                    break;
+
+                default:
+                    Dbg.Assert(false, "Invalid parameter set");
+                    break;
+            }
+
+            if (PassThru)
+            {
+                foreach(PSRepositoryInfo item in items)
+                {
+                    WriteObject(item);
+                }
+            }
+        }
+
+        private PSRepositoryInfo UpdateRepositoryStoreHelper(string repoName, Uri repoUrl, int repoPriority, bool repoTrusted)
+        {
+            if (repoUrl != null && !(repoUrl.Scheme == Uri.UriSchemeHttp || repoUrl.Scheme == Uri.UriSchemeHttps || repoUrl.Scheme == Uri.UriSchemeFtp || repoUrl.Scheme == Uri.UriSchemeFile))
+            {
+                throw new ArgumentException("Invalid url, must be one of the following Uri schemes: HTTPS, HTTP, FTP, File Based");
+            }
+
+            // check repoName can't contain * or just be whitespace
+            // remove trailing and leading whitespaces, and if Name is just whitespace Name should become null now and be caught by following condition
+            repoName = repoName.Trim(' ');
+            if (String.IsNullOrEmpty(repoName) || repoName.Contains("*"))
+            {
+                throw new ArgumentException("Name cannot be null/empty, contain asterisk or be just whitespace");
+            }
+
+            // check PSGallery URL is not trying to be set
+            if (repoName.Equals("PSGallery", StringComparison.OrdinalIgnoreCase) && repoUrl != null)
+            {
+                throw new ArgumentException("The PSGallery repository has a pre-defined URL.  Setting the -URL parmeter for this repository is not allowed, instead try running 'Register-PSResourceRepository -PSGallery'.");
+            }
+
+            // determine trusted value to pass in (true/false if set, null otherwise, hence the nullable bool variable)
+            bool? _trustedNullable = isSet ? new bool?(repoTrusted) : new bool?();
+
+            // determine if either 1 of 3 values are attempting to be set: URL, Priority, Trusted.
+            // if none are (i.e only Name parameter was provided, write error)
+            if(repoUrl == null && repoPriority == DefaultPriority && _trustedNullable == null)
+            {
+                // why not throw instead? and use wrapper catch's error id in test?
+                throw new ArgumentException("Either URL, Priority or Trusted parameters must be requested to be set");
+                // WriteError(new ErrorRecord(
+                //         new PSInvalidOperationException("Either URL, Priority or Trusted parameters must be requested to be set"),
+                //         "NoValuesBeingSet",
+                //         ErrorCategory.InvalidArgument,
+                //         this));
+                // return null;
+            }
+
+            WriteDebug("All required values to set repository provided, calling internal Update() API now");
+            if (!ShouldProcess(repoName, "Set repository's value(s) in repository store"))
+            {
+                return null;
+            }
+            return RepositorySettings.Update(repoName, repoUrl, repoPriority, _trustedNullable);
+        }
+        private PSRepositoryInfo NameParameterSetHelper(string repoName, Uri repoUrl, int repoPriority, bool repoTrusted)
+        {
+            // bool? _trustedNullable = isSet ? new bool?(_trusted) : new bool?(); //could move this to update helper tbh
+            return UpdateRepositoryStoreHelper(repoName, repoUrl, repoPriority, repoTrusted);
+        }
+
+        private List<PSRepositoryInfo> RepositoriesParameterSetHelper()
+        {
+            List<PSRepositoryInfo> reposUpdatedFromHashtable = new List<PSRepositoryInfo>();
+            foreach (Hashtable repo in Repositories)
+            {
+                if (!repo.ContainsKey("Name") || String.IsNullOrEmpty(repo["Name"].ToString()))
+                {
+                    WriteError(new ErrorRecord(
+                            new PSInvalidOperationException("Repository hashtable must contain Name key value pair"),
+                            "NullNameForRepositoriesParameterSetRepo",
+                            ErrorCategory.InvalidArgument,
+                            this));
+                    continue;
+                }
+
+                PSRepositoryInfo parsedRepoAdded = RepoValidationHelper(repo);
+                if (parsedRepoAdded != null)
+                {
+                    reposUpdatedFromHashtable.Add(parsedRepoAdded);
+                }
+            }
+            return reposUpdatedFromHashtable;
+        }
+
+        private PSRepositoryInfo RepoValidationHelper(Hashtable repo)
+        {
+            Uri repoURL = null;
+            if (repo.ContainsKey("Url")  && !Uri.TryCreate(repo["URL"].ToString(), UriKind.Absolute, out repoURL))
+            {
+                WriteError(new ErrorRecord(
+                    new PSInvalidOperationException("Invalid Url, unable to parse and create Uri"),
+                    "InvalidUrl",
+                    ErrorCategory.InvalidArgument,
+                    this));
+                return null;
+            }
+
+
+            // bool? _repoTrusted = repo.ContainsKey("Trusted") ? (bool?)repo["Trusted"] : null;
+            bool repoTrusted = false;
+            isSet = false;
+            if(repo.ContainsKey("Trusted"))
+            {
+                repoTrusted = (bool) repo["Trusted"];
+                isSet = true;
+            }
+            try
+            {
+                return NameParameterSetHelper(repo["Name"].ToString(),
+                    repoURL,
+                    repo.ContainsKey("Priority") ? Convert.ToInt32(repo["Priority"].ToString()) : DefaultPriority,
+                    repoTrusted);
+                    //or just directly call Update tbh?
+            }
+            catch (Exception e)
+            {
+                WriteError(new ErrorRecord(
+                        new PSInvalidOperationException(e.Message),
+                        "ErrorSettingIndividualRepoFromRepositories",
+                        ErrorCategory.InvalidArgument,
+                        this));
+                return null;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ test/SetPSResourceRepository.Tests.ps1
@@ -0,0 +1,130 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+Import-Module "$psscriptroot\PSGetTestUtils.psm1" -Force
+
+Describe "Test Register-PSResourceRepository" {
+    BeforeEach {
+        $PSGalleryName = Get-PSGalleryName
+        $PSGalleryURL = Get-PSGalleryLocation
+        Get-NewPSResourceRepositoryFile
+        $tmpDir1Path = Join-Path -Path $TestDrive -ChildPath "tmpDir1"
+        $tmpDir2Path = Join-Path -Path $TestDrive -ChildPath "tmpDir2"
+        $tmpDir3Path = Join-Path -Path $TestDrive -ChildPath "tmpDir3"
+        $tmpDirPaths = @($tmpDir1Path, $tmpDir2Path, $tmpDir3Path)
+        Get-NewTestDirs($tmpDirPaths)
+    }
+    AfterEach {
+        Get-RevertPSResourceRepositoryFile
+        $tmpDir1Path = Join-Path -Path $TestDrive -ChildPath "tmpDir1"
+        $tmpDir2Path = Join-Path -Path $TestDrive -ChildPath "tmpDir2"
+        $tmpDir3Path = Join-Path -Path $TestDrive -ChildPath "tmpDir3"
+        $tmpDirPaths = @($tmpDir1Path, $tmpDir2Path, $tmpDir3Path)
+        Get-RemoveTestDirs($tmpDirPaths)
+    }
+
+    It "set repository given Name and URL parameters" {
+        Register-PSResourceRepository -Name "testRepository" -URL $tmpDir1Path
+        Set-PSResourceRepository -Name "testRepository" -URL $tmpDir2Path
+        $res = Get-PSResourceRepository -Name "testRepository"
+        $res.Name | Should -Be "testRepository"
+        $res.URL | Should -Contain $tmpDir2Path
+        $res.Priority | Should -Be 50
+        $res.Trusted | Should -Be False
+    }
+
+    It "set repository given Name and Priority parameters" {
+        Register-PSResourceRepository -Name "testRepository" -URL $tmpDir1Path
+        Set-PSResourceRepository -Name "testRepository" -Priority 25
+        $res = Get-PSResourceRepository -Name "testRepository"
+        $res.Name | Should -Be "testRepository"
+        $res.URL | Should -Contain $tmpDir1Path
+        $res.Priority | Should -Be 25
+        $res.Trusted | Should -Be False
+    }
+
+    It "set repository given Name and Trusted parameters" {
+        Register-PSResourceRepository -Name "testRepository" -URL $tmpDir1Path
+        Set-PSResourceRepository -Name "testRepository" -Trusted
+        $res = Get-PSResourceRepository -Name "testRepository"
+        $res.Name | Should -Be "testRepository"
+        $res.URL | Should -Contain $tmpDir1Path
+        $res.Priority | Should -Be 50
+        $res.Trusted | Should -Be True
+    }
+
+    It "not set repository and write error given just Name parameter" {
+        Register-PSResourceRepository -Name "testRepository" -URL $tmpDir1Path
+        {Set-PSResourceRepository -Name "testRepository" -ErrorAction Stop} | Should -Throw -ErrorId "ErrorInNameParameterSet,Microsoft.PowerShell.PowerShellGet.Cmdlets.SetPSResourceRepository"
+    }
+
+    $testCases = @{Type = "contains *";     Name = "test*Repository"},
+                 @{Type = "is whitespace";  Name = " "}
+
+    It "not set repository and write error given Name <Type> (NameParameterSet)" -TestCases $testCases {
+        param($Type, $Name)
+
+        Register-PSResourceRepository -Name "testRepository" -URL $tmpDir1Path
+        {Set-PSResourceRepository -Name $Name -Priority 25 -ErrorAction Stop} | Should -Throw -ErrorId "ErrorInNameParameterSet,Microsoft.PowerShell.PowerShellGet.Cmdlets.SetPSResourceRepository"
+    }
+
+    It "set repositories with Repositories parameter" {
+        Unregister-PSResourceRepository -Name "PSGallery"
+        Register-PSResourceRepository -Name "testRepository1" -URL $tmpDir1Path
+        Register-PSResourceRepository -Name "testRepository2" -URL $tmpDir2Path
+        Register-PSResourceRepository -PSGallery
+
+        $hashtable1 = @{Name = "testRepository1"; URL = $tmpDir2Path};
+        $hashtable2 = @{Name = "testRepository2"; Priority = 25};
+        $hashtable3 = @{Name = "PSGallery"; Trusted = $True};
+        $arrayOfHashtables = $hashtable1, $hashtable2, $hashtable3
+
+        Set-PSResourceRepository -Repositories $arrayOfHashtables
+        $res = Get-PSResourceRepository -Name "testRepository1"
+        $res.Name | Should -Be "testRepository1"
+        $res.URL | Should -Contain $tmpDir2Path
+        $res.Priority | Should -Be 50
+        $res.Trusted | Should -Be False
+
+        $res2 = Get-PSResourceRepository -Name "testRepository2"
+        $res2.Name | Should -Be "testRepository2"
+        $res2.URL | Should -Contain $tmpDir2Path
+        $res2.Priority | Should -Be 25
+        $res2.Trusted | Should -Be False
+
+        $res3 = Get-PSResourceRepository -Name $PSGalleryName
+        $res3.Name | Should -Be $PSGalleryName
+        $res3.URL | Should -Contain $PSGalleryURL
+        $res3.Priority | Should -Be 50
+        $res3.Trusted | Should -Be True
+    }
+
+    It "not set and throw error for trying to set PSGallery URL (NameParameterSet)" {
+        Unregister-PSResourceRepository -Name "PSGallery"
+        Register-PSResourceRepository -PSGallery
+        {Set-PSResourceRepository -Name "PSGallery" -URL $tmpDir1Path -ErrorAction Stop} | Should -Throw -ErrorId "ErrorInNameParameterSet,Microsoft.PowerShell.PowerShellGet.Cmdlets.SetPSResourceRepository"
+    }
+
+    It "not set repository and throw error for trying to set PSGallery URL (RepositoriesParameterSet)" {
+        Unregister-PSResourceRepository -Name "PSGallery"
+        Register-PSResourceRepository -PSGallery
+
+        Register-PSResourceRepository -Name "testRepository" -URL $tmpDir1Path
+
+        $hashtable1 = @{Name = "PSGallery"; URL = $tmpDir1Path}
+        $hashtable2 = @{Name = "testRepository"; Priority = 25}
+        $arrayOfHashtables = $hashtable1, $hashtable2
+
+        Set-PSResourceRepository -Repositories $arrayOfHashtables -ErrorVariable err -ErrorAction SilentlyContinue
+        $err.Count | Should -Not -Be 0
+        $err[0].FullyQualifiedErrorId | Should -BeExactly "ErrorSettingIndividualRepoFromRepositories,Microsoft.PowerShell.PowerShellGet.Cmdlets.SetPSResourceRepository"
+
+        $res = Get-PSResourceRepository -Name "testRepository"
+        $res.URL | Should -Contain $tmpDir1Path
+        $res.Priority | Should -Be 25
+        $res.Trusted | Should -Be False
+    }
+
+
+
+}
