f94cd4517c59b2f113d3ef9ec7e545858d443a26
Amber Erickson <americks@microsoft.com>
2021-06-07 11:10:45 -0700

    Revert .psm1 (will create new PR for these changes)

--- src/PSModule.psm1
+++ src/PSModule.psm1
@@ -5,7 +5,16 @@ Set-StrictMode -Version Latest
 
 # Summary: PowerShellGet is supported on Windows PowerShell 5.1 or later and PowerShell 6.0+
 
-$script:Framework = 'netstandard2.0'
+$isCore = ($PSVersionTable.Keys -contains "PSEdition") -and ($PSVersionTable.PSEdition -ne 'Desktop')
+if ($isCore)
+{
+    $script:Framework = 'netstandard2.0'
+    $script:FrameworkToRemove = 'net472'
+    
+} else {
+    $script:Framework = 'net472'
+    $script:FrameworkToRemove = 'netstandard2.0'
+}
 
 # Set up some helper variables to make it easier to work with the module
 $script:PSModule = $ExecutionContext.SessionState.Module
@@ -13,6 +22,7 @@ $script:PSModuleRoot = $script:PSModule.ModuleBase
 $script:PSGet = 'PowerShellGet.dll'
 
 # CONSTRUCT A PATH TO THE CORRECT ASSEMBLY
+#$pathToAssembly = [io.path]::combine($PSScriptRoot,  $script:Framework, $script:PSGet)
 $pathToFramework = Join-Path -Path $script:PSModuleRoot -ChildPath $script:Framework
 $pathToAssembly = Join-Path -Path $pathToFramework -ChildPath $script:PSGet
 
