e6043d9997447006c2f1acba6fca4451ceff48f5
Anam Navied <anam.naviyou@gmail.com>
2021-08-05 17:08:11 -0400

    make support for Artifactory more generalized

--- src/code/FindHelper.cs
+++ src/code/FindHelper.cs
@@ -165,7 +165,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
             }
 
             // check if ADOFeed- for which searching for Name with wildcard has a different logic flow
-            if (repositoryUrl.ToString().Contains("pkgs.visualstudio.com"))
+            if (repositoryUrl.ToString().Contains("pkgs."))
             {
                 _isADOFeedRepository = true;
             }
