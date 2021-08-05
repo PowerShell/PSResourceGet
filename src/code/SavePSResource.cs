2c7e0df180caadf656f28494800425bf01edb7bd
Anam Navied <anam.naviyou@gmail.com>
2021-08-05 17:20:00 -0400

    add ShouldSupport implementation for Save-PSResource

--- src/code/SavePSResource.cs
+++ src/code/SavePSResource.cs
@@ -146,6 +146,12 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
 
         protected override void ProcessRecord()
         {
+            if (!ShouldProcess(string.Format("package to save: '{0}'", String.Join(", ", Name))))
+            {
+                WriteVerbose(string.Format("Save operation cancelled by user for packages: {0}", String.Join(", ", Name)));
+                return;
+            }
+
             var installHelper = new InstallHelper(updatePkg: false, savePkg: true, cmdletPassedIn: this);
 
             switch (ParameterSetName)
