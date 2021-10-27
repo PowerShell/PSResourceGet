59100793e4cb1adaf2c53f39c780afc00fb10302
Anam Navied <anam.naviyou@gmail.com>
2021-10-27 15:09:32 -0400

    fix typo

--- src/code/UninstallPSResource.cs
+++ src/code/UninstallPSResource.cs
@@ -115,7 +115,6 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
                     // PSResourceInfo object will always have:
                     // specific version, not version range
                     // Version and PrereleaseLabel (if any) as separate properties, i.e "1.0.0-alpha" case wouldn't be piped in
-
                     _prereleaseLabels[0] = InputObject.PrereleaseLabel;
                     if (!Utils.TryParseVersionOrVersionRange(version: InputObject.Version.ToString(),
                         versionRange: out _versionRange))
