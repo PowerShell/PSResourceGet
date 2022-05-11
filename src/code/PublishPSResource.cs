61649e6a6b015200e3ff0d166c6a03b69fb89be5
Anam Navied <anam.naviyou@gmail.com>
2022-05-11 12:38:08 -0400

    typo fix

--- src/code/PublishPSResource.cs
+++ src/code/PublishPSResource.cs
@@ -635,7 +635,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
                 }
                 else if(repository.Uri.Scheme == Uri.UriSchemeFile && !Directory.Exists(repository.Uri.LocalPath))
                 {
-                    //TODO: would this include localhost?
+                    //TODO: Anam would this include localhost?
                     var message = String.Format("The repository '{0}' with uri: {1} is not a valid folder path which exists. If providing a file based repository, provide a repository with a path that exists.", Repository, repository.Uri.AbsoluteUri);
                     var ex = new ArgumentException(message);
                     var fileRepositoryPathDoesNotExistError = new ErrorRecord(ex, "repositoryPathDoesNotExist", ErrorCategory.ObjectNotFound, null);
