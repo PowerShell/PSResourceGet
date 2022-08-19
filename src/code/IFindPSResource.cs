2e83766af98b76d646307f544ff5c4e4c0795bf6
Anam Navied <anam.naviyou@gmail.com>
2022-08-19 15:41:28 -0400

    interface design for Find

---
+++ src/code/IFindPSResource.cs
@@ -0,0 +1,118 @@
+using Microsoft.PowerShell.PowerShellGet.UtilClasses;
+using NuGet.Versioning;
+
+public interface IFindPSResource
+{
+    #region Properties
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Find method which allows for searching for all packages from a repository and returns latest version for each.
+    /// Examples: Search -Repository PSGallery
+    /// </summary>
+    void FindAll(string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for packages with tag(s) from a repository and returns latest version for each.
+    /// Examples: Search -Tag "JSON" -Repository PSGallery
+    /// </summary>
+    void FindTags(string[] tags, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for packages with resource type specified from a repository and returns latest version for each.
+    /// Name: supports wildcards
+    /// Type: Module, Script, Command, DSCResource (can take multiple)
+    /// Examples: Search -Type Module -Repository PSGallery
+    ///           Search "Az*" -Type Module -Repository PSGallery
+    /// </summary>
+    void FindTypes(ResourceType packageResourceType, string packageName, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for command names and returns latest version of matching packages.
+    /// Name: supports wildcards
+    /// Examples: Search -Name "Command1", "Command2" -Repository PSGallery
+    /// </summary>
+    void FindCommandName(string[] commandNames, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for DSC Resource names and returns latest version of matching packages.
+    /// Name: supports wildcards
+    /// Examples: Search -Name "DSCResource1", "DSCResource2" -Repository PSGallery
+    void FindDSCResourceName(string[] dscResourceName, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for single name and returns latest version.
+    /// Name: no wildcard support
+    /// Examples: Search "PowerShellGet"
+    /// </summary>
+    void FindName(string packageName, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for single name with wildcards and returns latest version.
+    /// Name: supports wildcards
+    /// Examples: Search "PowerShell*"
+    /// </summary>
+    void FindNameGlobbing(string packageName, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for single name with version range.
+    /// Name: no wildcard support
+    /// Version: supports wildcards
+    /// Examples: Search "PowerShellGet" "[3.0.0.0, 5.0.0.0]"
+    ///           Search "PowerShellGet" "3.*"
+    /// </summary>
+    void FindVersionGlobbing(string packageName, VersionRange versionRange, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for single name with specific version.
+    /// Name: no wildcard support
+    /// Version: no wildcard support
+    /// Examples: Search "PowerShellGet" "3.0.0.0"
+    /// </summary>
+    void FindVersion(string packageName, NuGetVersion version, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for single name with wildcards with version range.
+    /// Name: supports wildcards
+    /// Version: support wildcards
+    /// Examples: Search "PowerShell*" "[3.0.0.0, 5.0.0.0]"
+    ///           Search "PowerShell*" "3.*"
+    /// </summary>
+    void FindNameGlobbingAndVersionGlobbing(string packageName, VersionRange versionRange, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for single name with wildcards with specific version.
+    /// Name: supports wildcards
+    /// Version: no wildcard support
+    /// Examples: Search "PowerShell*" "3.0.0.0"
+    /// </summary>
+    void FindNameGlobbingAndVersion(string packageName, NuGetVersion version, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for multiple names and returns latest version for each.
+    /// Name: supports wildcards
+    /// Examples: Search "PowerShellGet", "Package*", "PSReadLine"
+    /// </summary>
+    void FindNamesGlobbing(string[] packageNames, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for multiple names with specific version.
+    /// Name: supports wildcards
+    /// Version: no wildcard support
+    /// Examples: Search "PowerShellGet", "Package*", "PSReadLine" "3.0.0.0"
+    /// </summary>
+    void FindNamesGlobbingAndVersion(string[] packageNames, NuGetVersion version, string repositoryName);
+
+    /// <summary>
+    /// Find method which allows for searching for multiple names with version range.
+    /// Name: supports wildcards
+    /// Version: support wildcards
+    /// Examples: Search "PowerShellGet", "Package*", "PSReadLine" "[3.0.0.0, 5.0.0.0]"
+    ///           Search "PowerShellGet", "Package*", "PSReadLine" "3.*"
+    /// </summary>
+    void FindNamesGlobbingAndVersionGlobbing(string[] packageNames, VersionRange versionRange, string repositoryName); 
+
+    #endregion
+}
