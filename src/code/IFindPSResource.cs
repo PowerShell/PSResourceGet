980ba6bd04f433c1ba96d530d9764b852ed3add9
Alyssa Vu <alyssavu@microsoft.com>
2022-08-29 17:51:36 -0400

    add API call examples

--- src/code/IFindPSResource.cs
+++ src/code/IFindPSResource.cs
@@ -8,12 +8,18 @@ public interface IFindPSResource
     /// <summary>
     /// Find method which allows for searching for all packages from a repository and returns latest version for each.
     /// Examples: Search -Repository PSGallery
+    /// API call: 
+    /// - No prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion
+    /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsAbsoluteLatestVersion&includePrerelease=true
     /// </summary>
     PSResourceInfo FindAll(PSRepositoryInfo repository, bool includePrerelease);
 
     /// <summary>
     /// Find method which allows for searching for packages with tag(s) from a repository and returns latest version for each.
     /// Examples: Search -Tag "JSON" -Repository PSGallery
+    /// API call: 
+    /// - No prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='tag:JSON'
+    /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsAbsoluteLatestVersion&searchTerm='tag:JSON'&includePrerelease=true
     /// </summary>
     PSResourceInfo FindTags(string[] tags, PSRepositoryInfo repository, bool includePrerelease);
 
@@ -23,6 +29,8 @@ public interface IFindPSResource
     /// Type: Module, Script, Command, DSCResource (can take multiple)
     /// Examples: Search -Type Module -Repository PSGallery
     ///           Search "Az*" -Type Module -Repository PSGallery
+    /// - No prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='tag:Module'
+    /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsAbsoluteLatestVersion&searchTerm='tag:Module'&includePrerelease=true
     /// </summary>
     PSResourceInfo FindTypes(ResourceType packageResourceType, string packageName, PSRepositoryInfo repository, bool includePrerelease);
 
@@ -43,6 +51,9 @@ public interface IFindPSResource
     /// Find method which allows for searching for single name and returns latest version.
     /// Name: no wildcard support
     /// Examples: Search "PowerShellGet"
+    /// API call: 
+    /// - No prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='PowerShellGet'
+    /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsAbsoluteLatestVersion&searchTerm='PowerShellGet'&includePrerelease=true
     /// </summary>
     PSResourceInfo FindName(string packageName, PSRepositoryInfo repository, bool includePrerelease);
 
@@ -50,6 +61,9 @@ public interface IFindPSResource
     /// Find method which allows for searching for single name with wildcards and returns latest version.
     /// Name: supports wildcards
     /// Examples: Search "PowerShell*"
+    /// API call: 
+    /// - No prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='az*'
+    /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsAbsoluteLatestVersion&searchTerm='az*'&includePrerelease=true
     /// </summary>
     PSResourceInfo FindNameGlobbing(string packageName, PSRepositoryInfo repository, bool includePrerelease);
 
@@ -66,7 +80,8 @@ public interface IFindPSResource
     /// Find method which allows for searching for single name with specific version.
     /// Name: no wildcard support
     /// Version: no wildcard support
-    /// Examples: Search "PowerShellGet" "3.0.0.0"
+    /// Examples: Search "PowerShellGet" "2.2.5"
+    /// API call: http://www.powershellgallery.com/api/v2/Packages(Id='PowerShellGet', Version='2.2.5')
     /// </summary>
     PSResourceInfo FindVersion(string packageName, NuGetVersion version, PSRepositoryInfo repository);
 
@@ -91,6 +106,9 @@ public interface IFindPSResource
     /// Find method which allows for searching for multiple names and returns latest version for each.
     /// Name: supports wildcards
     /// Examples: Search "PowerShellGet", "Package*", "PSReadLine"
+    /// API call: 
+    /// - No prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='PSReadLine, Package*'
+    /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsAbsoluteLatestVersion&searchTerm='PSReadLine, Package*'&includePrerelease=true
     /// </summary>
     PSResourceInfo FindNamesGlobbing(string[] packageNames, PSRepositoryInfo repository, bool includePrerelease);
 
