c002bc2427f843d93578a880b982a425fb14c516
Alyssa Vu <49544763+alyssa1303@users.noreply.github.com>
2022-09-29 13:14:54 -0400

    Automate updating `CHANGELOG.md` file (#817)

---
+++ tool/setupReleaseTools.ps1
@@ -0,0 +1,17 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+param(
+    [Parameter(Mandatory)]
+    [string]$Token
+)
+
+Write-Host "Install and import PowerShell modules"
+Set-PSResourceRepository -Name PSGallery -Trusted | Out-Null
+Install-Module -Name PowerShellForGitHub -Scope CurrentUser -Force
+Import-Module $PSScriptRoot/releaseTools.psm1
+
+Write-Host "Setup authentication"
+Set-GitHubConfiguration -SuppressTelemetryReminder
+$password = ConvertTo-SecureString -String $Token -AsPlainText -Force
+Set-GitHubAuthentication -Credential (New-Object System.Management.Automation.PSCredential ("token", $password))
