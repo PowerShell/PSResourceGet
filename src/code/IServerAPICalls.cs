16657f4a8b49b9c9c7d5caa2ad210fe290af9723
Amber Erickson <americks@microsoft.com>
2022-10-04 13:42:45 -0700

    Fill out code for V2ServerAPICalls

--- src/code/IServerAPICalls.cs
+++ src/code/IServerAPICalls.cs
@@ -1,5 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using Microsoft.PowerShell.PowerShellGet.UtilClasses;
-using NuGet.Versioning;
 
 public interface IServerAPICalls
 {
@@ -12,7 +14,7 @@ public interface IServerAPICalls
     /// API call: 
     /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsAbsoluteLatestVersion&includePrerelease=true
     /// </summary>
-    string FindAllWithNoPrerelease(PSRepositoryInfo repository, out string[] errRecord);
+    string FindAllWithNoPrerelease(PSRepositoryInfo repository, out string errRecord);
 
     /// <summary>
     /// Find method which allows for searching for all packages from a repository and returns latest version for each.
@@ -20,7 +22,7 @@ public interface IServerAPICalls
     /// API call: 
     /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsAbsoluteLatestVersion&includePrerelease=true
     /// </summary>
-    string FindAllWithPrerelease(PSRepositoryInfo repository, out string[] errRecord);
+    string FindAllWithPrerelease(PSRepositoryInfo repository, out string errRecord);
 
     /// <summary>
     /// Find method which allows for searching for packages with tag(s) from a repository and returns latest version for each.
@@ -28,7 +30,7 @@ public interface IServerAPICalls
     /// API call: 
     /// - No prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='tag:JSON'
     /// </summary>
-    string FindTagsWithNoPrerelease(string[] tags, PSRepositoryInfo repository, out string[] errRecord);
+    string FindTagsWithNoPrerelease(string[] tags, PSRepositoryInfo repository, out string errRecord);
 
     /// <summary>
     /// Find method which allows for searching for packages with tag(s) from a repository and returns latest version for each.
@@ -36,7 +38,7 @@ public interface IServerAPICalls
     /// API call: 
     /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsAbsoluteLatestVersion&searchTerm='tag:JSON'&includePrerelease=true
     /// </summary>
-    string FindTagsWithPrerelease(string[] tags, PSRepositoryInfo repository, out string[] errRecord);
+    string FindTagsWithPrerelease(string[] tags, PSRepositoryInfo repository, out string errRecord);
 
 
     /// <summary>
@@ -49,7 +51,7 @@ public interface IServerAPICalls
     /// TODO Note: searchTerm is tokenized by whitespace.
     /// - No prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='Az* tag:PSModule'
     /// </summary>
-    string FindTypesWithNoPrerelease(ResourceType packageResourceType, string packageName, PSRepositoryInfo repository, out string[] errRecord);
+    string FindTypesWithNoPrerelease(ResourceType packageResourceType, string packageName, PSRepositoryInfo repository, out string errRecord);
 
     /// <summary>
     /// Find method which allows for searching for packages with resource type specified from a repository and returns latest version for each.
@@ -61,7 +63,7 @@ public interface IServerAPICalls
     /// TODO Note: searchTerm is tokenized by whitespace.
     /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='az* tag:PSScript'&includePrerelease=true
     /// </summary>
-    string FindTypesWithPrerelease(ResourceType packageResourceType, string packageName, PSRepositoryInfo repository, out string[] errRecord);
+    string FindTypesWithPrerelease(ResourceType packageResourceType, string packageName, PSRepositoryInfo repository, out string errRecord);
 
 
     /// <summary>
@@ -70,7 +72,7 @@ public interface IServerAPICalls
     /// Examples: Search -Name "Command1", "Command2" -Repository PSGallery
     /// - No prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='Az* tag:PSCommand_Command1 tag:PSCommand_Command2'
     /// </summary>
-    string FindCommandNameWithNoPrerelease(string[] commandNames, PSRepositoryInfo repository, out string[] errRecord);
+    string FindCommandNameWithNoPrerelease(string[] commandNames, PSRepositoryInfo repository, out string errRecord);
   
     /// <summary>
     /// Find method which allows for searching for command names and returns latest version of matching packages.
@@ -78,24 +80,7 @@ public interface IServerAPICalls
     /// Examples: Search -Name "Command1", "Command2" -Repository PSGallery
     /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='Az* tag:PSCommand_Command1 tag:PSCommand_Command2'&includePrerelease=true
     /// </summary>
-    string FindCommandNameWithPrerelease(string[] commandNames, PSRepositoryInfo repository, out string[] errRecord);
-
-    /// <summary>
-    /// Find method which allows for searching for DSC Resource names and returns latest version of matching packages.
-    /// Name: Support wildcards.
-    /// Examples: Search -Name "DSCResource1", "DSCResource2" -Repository PSGallery
-    /// - No prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='Az* tag:PSDSCResource_DSCResource1 tag:PSDSCResource_DSCResource2'
-    /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='Az* tag:PSDSCResource_DSCResource1 tag:PSDSCResource_DSCResource2'&includePrerelease=true
-    string FindDSCResourceNameWithNoPrerelease(string[] dscResourceName, PSRepositoryInfo repository, out string[] errRecord);
-
-
-    /// <summary>
-    /// Find method which allows for searching for DSC Resource names and returns latest version of matching packages.
-    /// Name: Support wildcards.
-    /// Examples: Search -Name "DSCResource1", "DSCResource2" -Repository PSGallery
-    /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsLatestVersion&searchTerm='Az* tag:PSDSCResource_DSCResource1 tag:PSDSCResource_DSCResource2'&includePrerelease=true
-    string FindDSCResourceNameWithPrerelease(string[] dscResourceName, PSRepositoryInfo repository, out string[] errRecord);
-   
+    string FindCommandNameWithPrerelease(string[] commandNames, PSRepositoryInfo repository, out string errRecord);
 
     /// <summary>
     /// Find method which allows for searching for single name and returns latest version.
@@ -106,7 +91,7 @@ public interface IServerAPICalls
     /// - Include prerelease: http://www.powershellgallery.com/api/v2/FindPackagesById()?id='PowerShellGet'
     /// Implementation Note: Need to filter further for latest version (prerelease or non-prerelease dependening on user preference)
     /// </summary>
-    string FindName(string packageName, PSRepositoryInfo repository, out string[] errRecord);
+    string FindName(string packageName, PSRepositoryInfo repository, out string errRecord);
 
     /// <summary>
     /// Find method which allows for searching for single name with version range.
@@ -117,7 +102,7 @@ public interface IServerAPICalls
     /// API Call: http://www.powershellgallery.com/api/v2/FindPackagesById()?id='PowerShellGet'
     /// Implementation note: Returns all versions, including prerelease ones. Later (in the API client side) we'll do filtering on the versions to satisfy what user provided.
     /// </summary>
-    string FindNameGlobbingWithNoPrerelease(string packageName, PSRepositoryInfo repository, out string[] errRecord);
+    string FindNameGlobbingWithNoPrerelease(string packageName, PSRepositoryInfo repository, out string errRecord);
 
 /// <summary>
     /// Find method which allows for searching for single name with wildcards and returns latest version.
@@ -127,7 +112,7 @@ public interface IServerAPICalls
     /// - Include prerelease: http://www.powershellgallery.com/api/v2/Search()?$filter=IsAbsoluteLatestVersion&searchTerm='az*'&includePrerelease=true
     /// Implementation Note: filter additionally and verify ONLY package name was a match.
     /// </summary>
-    string FindNameGlobbingWithPrerelease(string packageName, PSRepositoryInfo repository, out string[] errRecord);
+    string FindNameGlobbingWithPrerelease(string packageName, PSRepositoryInfo repository, out string errRecord);
     {
 
 
@@ -138,7 +123,7 @@ public interface IServerAPICalls
     /// Examples: Search "PowerShellGet" "2.2.5"
     /// API call: http://www.powershellgallery.com/api/v2/Packages(Id='PowerShellGet', Version='2.2.5')
     /// </summary>
-    string FindVersionGlobbing(string packageName, VersionRange versionRange, PSRepositoryInfo repository, out string[] errRecord);
+    string FindVersionGlobbing(string packageName, VersionRange versionRange, PSRepositoryInfo repository, out string errRecord);
     
     /// <summary>
     /// *** we will not support this scenario ***
@@ -194,7 +179,7 @@ public interface IServerAPICalls
     /// Examples: Search "PowerShellGet" "2.2.5"
     /// API call: http://www.powershellgallery.com/api/v2/Packages(Id='PowerShellGet', Version='2.2.5')
     /// </summary>
-    string FindVersion(string packageName, NuGetVersion version, PSRepositoryInfo repository, out string[] errRecord);
+    string FindVersion(string packageName, NuGetVersion version, PSRepositoryInfo repository, out string errRecord);
     
 
     /// <summary>
@@ -204,7 +189,7 @@ public interface IServerAPICalls
     /// Implementation Note: if prerelease: call IFindPSResource.FindName()
     ///                      if not prerelease: https://www.powershellgallery.com/api/v2/package/Id (Returns latest stable)
     /// </summary>
-    string InstallName(string packageName, PSRepositoryInfo repository, out string[] errRecord);
+    string InstallName(string packageName, PSRepositoryInfo repository, out string errRecord);
 
     /// <summary>
     /// Installs package with specific name and version.
@@ -214,7 +199,7 @@ public interface IServerAPICalls
     ///           Install "PowerShellGet" -Version "3.0.0-beta16"
     /// API Call: https://www.powershellgallery.com/api/v2/package/Id/version (version can be prerelease)
     /// </summary>    
-    string InstallVersion(string packageName, NuGetVersion version, PSRepositoryInfo repository, out string[] errRecord);
+    string InstallVersion(string packageName, NuGetVersion version, PSRepositoryInfo repository, out string errRecord);
 
     #endregion
 }
