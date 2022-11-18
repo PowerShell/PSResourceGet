67f631a2e7fa9e5e95bdede78c24b8225a11a777
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2022-11-18 20:39:09 +0100

    Stripped out ArgumentTransformation

--- src/code/SetPSResourceRepository.cs
+++ src/code/SetPSResourceRepository.cs
@@ -86,7 +86,6 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
         /// Specifies vault and secret names as PSCredentialInfo for the repository.
         /// </summary>
         [Parameter(ParameterSetName = NameParameterSet)]
-        [PSCredentialInfoTransform()]
         public PSCredentialInfo CredentialInfo { get; set; }
 
         /// <summary>
