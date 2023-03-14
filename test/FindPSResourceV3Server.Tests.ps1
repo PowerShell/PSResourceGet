b3c69f22bf28ccd78f12272c9dabc73d1e417573
Anam Navied <anam.naviyou@gmail.com>
2023-03-14 14:40:30 -0400

    add dep test for V3 find

--- test/FindPSResourceV3Server.Tests.ps1
+++ test/FindPSResourceV3Server.Tests.ps1
@@ -90,58 +90,58 @@ Describe 'Test HTTP Find-PSResource for V2 Server Protocol' {
         $resWithPrerelease.Count | Should -BeGreaterOrEqual $resWithoutPrerelease.Count
     }
 
-    # It "find resource and its dependency resources with IncludeDependencies parameter" {
-    #     # FindName() with deps
-    #     $resWithoutDependencies = Find-PSResource -Name "TestModuleWithDependencyE" -Repository $NuGetGalleryName
-    #     $resWithoutDependencies.Count | Should -Be 1
-    #     $resWithoutDependencies.Name | Should -Be "TestModuleWithDependencyE"
+    It "find resource and its dependency resources with IncludeDependencies parameter" {
+        # FindName() with deps
+        $resWithoutDependencies = Find-PSResource -Name "TestModuleWithDependencyE" -Repository $NuGetGalleryName
+        $resWithoutDependencies.Count | Should -Be 1
+        $resWithoutDependencies.Name | Should -Be "TestModuleWithDependencyE"
 
-    #     # TestModuleWithDependencyE has the following dependencies:
-    #     # TestModuleWithDependencyC <= 1.0.0.0
-    #     #    TestModuleWithDependencyB >= 1.0.0.0
-    #     #    TestModuleWithDependencyD <= 1.0.0.0
+        # TestModuleWithDependencyE has the following dependencies:
+        # TestModuleWithDependencyC <= 1.0.0.0
+        #    TestModuleWithDependencyB >= 1.0.0.0
+        #    TestModuleWithDependencyD <= 1.0.0.0
 
-    #     $resWithDependencies = Find-PSResource -Name "TestModuleWithDependencyE" -IncludeDependencies -Repository $NuGetGalleryName
-    #     $resWithDependencies.Count | Should -BeGreaterThan $resWithoutDependencies.Count
+        $resWithDependencies = Find-PSResource -Name "TestModuleWithDependencyE" -IncludeDependencies -Repository $NuGetGalleryName
+        $resWithDependencies.Count | Should -BeGreaterThan $resWithoutDependencies.Count
 
-    #     $foundParentPkgE = $false
-    #     $foundDepB = $false
-    #     $foundDepBCorrectVersion = $false
-    #     $foundDepC = $false
-    #     $foundDepCCorrectVersion = $false
-    #     $foundDepD = $false
-    #     $foundDepDCorrectVersion = $false
-    #     foreach ($pkg in $resWithDependencies)
-    #     {
-    #         if ($pkg.Name -eq "TestModuleWithDependencyE")
-    #         {
-    #             $foundParentPkgE = $true
-    #         }
-    #         elseif ($pkg.Name -eq "TestModuleWithDependencyC")
-    #         {
-    #             $foundDepC = $true
-    #             $foundDepCCorrectVersion = [System.Version]$pkg.Version -le [System.Version]"1.0.0.0"
-    #         }
-    #         elseif ($pkg.Name -eq "TestModuleWithDependencyB")
-    #         {
-    #             $foundDepB = $true
-    #             $foundDepBCorrectVersion = [System.Version]$pkg.Version -ge [System.Version]"3.0.0.0"
-    #         }
-    #         elseif ($pkg.Name -eq "TestModuleWithDependencyD")
-    #         {
-    #             $foundDepD = $true
-    #             $foundDepDCorrectVersion = [System.Version]$pkg.Version -le [System.Version]"1.0.0.0"
-    #         }
-    #     }
+        $foundParentPkgE = $false
+        $foundDepB = $false
+        $foundDepBCorrectVersion = $false
+        $foundDepC = $false
+        $foundDepCCorrectVersion = $false
+        $foundDepD = $false
+        $foundDepDCorrectVersion = $false
+        foreach ($pkg in $resWithDependencies)
+        {
+            if ($pkg.Name -eq "TestModuleWithDependencyE")
+            {
+                $foundParentPkgE = $true
+            }
+            elseif ($pkg.Name -eq "TestModuleWithDependencyC")
+            {
+                $foundDepC = $true
+                $foundDepCCorrectVersion = [System.Version]$pkg.Version -le [System.Version]"1.0.0.0"
+            }
+            elseif ($pkg.Name -eq "TestModuleWithDependencyB")
+            {
+                $foundDepB = $true
+                $foundDepBCorrectVersion = [System.Version]$pkg.Version -ge [System.Version]"3.0.0.0"
+            }
+            elseif ($pkg.Name -eq "TestModuleWithDependencyD")
+            {
+                $foundDepD = $true
+                $foundDepDCorrectVersion = [System.Version]$pkg.Version -le [System.Version]"1.0.0.0"
+            }
+        }
 
-    #     $foundParentPkgE | Should -Be $true
-    #     $foundDepC | Should -Be $true
-    #     $foundDepCCorrectVersion | Should -Be $true
-    #     $foundDepB | Should -Be $true
-    #     $foundDepBCorrectVersion | Should -Be $true
-    #     $foundDepD | Should -Be $true
-    #     $foundDepDCorrectVersion | Should -Be $true
-    # }
+        $foundParentPkgE | Should -Be $true
+        $foundDepC | Should -Be $true
+        $foundDepCCorrectVersion | Should -Be $true
+        $foundDepB | Should -Be $true
+        $foundDepBCorrectVersion | Should -Be $true
+        $foundDepD | Should -Be $true
+        $foundDepDCorrectVersion | Should -Be $true
+    }
 
     # It "find resources only with Tag parameter" {
     #     $resWithEitherExpectedTag = @("NetworkingDsc", "DSCR_FileContent", "SS.PowerShell")
