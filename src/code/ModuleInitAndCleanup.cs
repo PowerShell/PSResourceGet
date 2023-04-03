b5008f93bcff6c554f8719a56a26cafb415dcc8a
Aditya Patwardhan <adityap@microsoft.com>
2023-04-03 14:12:38 -0700

    Load all dependencies in an assembly load context

--- src/code/ModuleInitAndCleanup.cs
+++ src/code/ModuleInitAndCleanup.cs
@@ -1,14 +1,30 @@
 using System;
+using System.Collections.Generic;
 using System.Management.Automation;
 using System.IO;
 using System.Reflection;
 
 namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
 {
-#if NET472
     public class UnsafeAssemblyHandler : IModuleAssemblyInitializer, IModuleAssemblyCleanup
     {
-        private static readonly string s_asmLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private static readonly Assembly s_self;
+        private static readonly string s_dependencyFolder;
+        private static readonly HashSet<string> s_dependencies;
+        private static readonly AssemblyLoadContextProxy s_proxy;
+
+        static UnsafeAssemblyHandler()
+        {
+            s_self = Assembly.GetExecutingAssembly();
+            s_dependencyFolder = Path.Combine(Path.GetDirectoryName(s_self.Location), "dependencies");
+            s_dependencies = new HashSet<string>(StringComparer.Ordinal);
+            s_proxy = AssemblyLoadContextProxy.CreateLoadContext("powershellget-load-context");
+
+            foreach (string filePath in Directory.EnumerateFiles(s_dependencyFolder, "*.dll"))
+            {
+                s_dependencies.Add(AssemblyName.GetAssemblyName(filePath).FullName);
+            }
+        }
 
         public void OnImport()
         {
@@ -20,30 +36,68 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
             AppDomain.CurrentDomain.AssemblyResolve -= HandleAssemblyResolve;
         }
 
+        private static bool IsAssemblyMatching(AssemblyName assemblyName, Assembly requestingAssembly)
+        {
+            // The requesting assembly is always available in .NET, but could be null in .NET Framework.
+            // - When the requesting assembly is available, we check whether the loading request came from this
+            //   module, so as to make sure we only act on the request from this module.
+            // - When the requesting assembly is not available, we just have to depend on the assembly name only.
+            return requestingAssembly is not null
+                ? requestingAssembly == s_self && s_dependencies.Contains(assemblyName.FullName)
+                : s_dependencies.Contains(assemblyName.FullName);
+        }
+
         private static Assembly HandleAssemblyResolve(object sender, ResolveEventArgs args)
         {
             var requiredAssembly = new AssemblyName(args.Name);
 
-            string possibleAssembly = Path.Combine(s_asmLocation, $"{requiredAssembly.Name}.dll");
-
-            AssemblyName bundledAssembly = null;
-
-            try
+            if (IsAssemblyMatching(requiredAssembly, args.RequestingAssembly))
             {
-                bundledAssembly = AssemblyName.GetAssemblyName(possibleAssembly);
-            }
-            catch
-            {
-                return null;
-            }
+                string possibleAssembly = Path.Combine(s_dependencyFolder, $"{requiredAssembly.Name}.dll");
 
-            if (bundledAssembly.Version < requiredAssembly.Version)
-            {
-                return null;
+                if (File.Exists(possibleAssembly))
+                {
+                    // - In .NET, load the assembly into the custom assembly load context.
+                    // - In .NET Framework, assembly conflict is not a problem, so we load the assembly
+                    //   by 'Assembly.LoadFrom', the same as what powershell.exe would do.
+                    return s_proxy is not null
+                        ? s_proxy.LoadFromAssemblyPath(possibleAssembly)
+                        : Assembly.LoadFrom(possibleAssembly);
+                }
             }
 
-            return Assembly.LoadFrom(possibleAssembly);
+            return null;
         }
     }
-#endif
+
+    internal class AssemblyLoadContextProxy
+    {
+        private readonly object _customContext;
+        private readonly MethodInfo _loadFromAssemblyPath;
+
+        private AssemblyLoadContextProxy(string loadContextName)
+        {
+            var alc = typeof(object).Assembly.GetType("System.Runtime.Loader.AssemblyLoadContext");
+            var ctor = alc.GetConstructor(new[] { typeof(string), typeof(bool) });
+            _loadFromAssemblyPath = alc.GetMethod("LoadFromAssemblyPath", new[] { typeof(string) });
+            _customContext = ctor.Invoke(new object[] { loadContextName, false });
+        }
+
+        internal Assembly LoadFromAssemblyPath(string assemblyPath)
+        {
+            return (Assembly) _loadFromAssemblyPath.Invoke(_customContext, new[] { assemblyPath });
+        }
+
+        internal static AssemblyLoadContextProxy CreateLoadContext(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            return Environment.Version.Major > 4
+                ? new AssemblyLoadContextProxy(name)
+                : null;
+        }
+    }
 }
