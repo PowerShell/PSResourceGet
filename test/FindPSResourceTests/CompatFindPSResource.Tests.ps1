0cf337696d7bb13b38480653904eb8ecee8806d0
alerickson <25858831+alerickson@users.noreply.github.com>
2023-04-12 18:57:43 -0700

    Update find and get tests

--- test/FindPSResourceTests/CompatFindPSResource.Tests.ps1
+++ test/FindPSResourceTests/CompatFindPSResource.Tests.ps1
@@ -25,123 +25,112 @@ Describe 'Test CompatPowerShellGet: Find-PSResource' -tags 'CI' {
         Get-RevertPSResourceRepositoryFile
     }
 
-        It "Find-Module without any parameter values" {
-            $psgetItemInfo = Find-Module -Repository PSGallery
-            $psgetItemInfo.Count -ge 1 | Should -Be $true 
-        }
+    It "Find-Module without any parameter values" {
+        $psgetItemInfo = Find-Module -Repository PSGallery
+        $psgetItemInfo.Count -ge 1 | Should -Be $true 
+    }
 
-        It "Find a specific module" {
-            $res = Find-Module "testModule99" -Repository PSGallery
-            $res | Should -Not -BeNullOrEmpty 
-            $res.Name | Should -Be "testModule99"
-        }
+    It "Find a specific module" {
+        $res = Find-Module "testModule99" -Repository PSGallery
+        $res | Should -Not -BeNullOrEmpty 
+        $res.Name | Should -Be "testModule99"
+    }
+
+    It "Find-Module with range wildcards" {
+        $res = Find-Module -Name "TestModule9*" -Repository PSGallery
+        $res | Should -Not -BeNullOrEmpty 
+        $res.Name | Should -Be "TestModule99"
+    }
+
+    It "Find not available module with wildcards" {
+        $res = Find-Module -Name "TestModule5test*" -Repository PSGallery
+        $res | Should -BeNullOrEmpty
+    }
+
+    It "Find-Module with min version" {
+        $res = Find-Module TestModule99 -MinimumVersion 0.0.3 -Repository PSGallery
+        $res.Name | Should -Contain "TestModule99" 
+        $res | ForEach-Object { $_.Version -ge [Version]"0.0.3" | Should -Be $true }
+    }
+
+    It "Find-Module with min version not available" {
+        $res = Find-Module TestModule99 -MinimumVersion 10.0 -Repository PSGallery
+        $res | Should -BeNullOrEmpty
+    }
+
+    It "Find-Module with required version not available" {
+        $res = Find-Module TestModule99 -RequiredVersion 12.0 -Repository PSGallery -ErrorVariable ev -ErrorAction SilentlyContinue
+        $res | Should -BeNullOrEmpty
+    }
+
+    It "Find-Module with required version" {
+        $res = Find-Module TestModule99 -RequiredVersion 0.0.2 -Repository PSGallery
+        $res | Should -Not -BeNullOrEmpty
+        $res.Name | Should -Be "TestModule99"
+        $res.Version -eq [Version]"0.0.2" | Should -Be $true
+    }
+
+    It "Find-Module with multiple module names and required version" {
+        $res = Find-Module TestModuleWithDependencyB, TestModuleWithDependencyC -RequiredVersion 3.0 -Repository PSGallery
+        $res.Count -eq 2 | Should -Be $true 
+        $res | ForEach-Object { $_.Version -eq [Version]"3.0" | Should -Be $true }
+    }
+
+    It "Find-Module with module name and minimum version" {
+        $res = Find-Module TestModule99 -MinimumVersion 0.5 -Repository PSGallery
+        $res.Count -eq 0 | Should -Be $true
+    }
+
+    It "Find-Module with multiple module names and minimum version" {
+        $res = Find-Module TestModule99, TestModuleWithDependencyB -MinimumVersion 0.5 -Repository PSGallery
+        $res.Count -eq 2 | Should -Be $true 
+        $res.Name | Should -Not -Contain "TestModule99"
+        $res | ForEach-Object { $_.Version -ge [Version]"0.5" | Should -Be $true }
+    }
     
-        It "Find-Module with range wildcards" {
-            $res = Find-Module -Name "TestModule9*" -Repository PSGallery
-            $res | Should -Not -BeNullOrEmpty 
-            $res.Name | Should -Be "TestModule99"
-        }
+    It "Find-Module with wildcard name and minimum version" {
+        $res = Find-Module TestModule9* -MinimumVersion 0.0.3 -Repository PSGallery -ErrorAction SilentlyContinue -ErrorVariable err
+        $err | Should -Not -BeNullOrEmpty
+        $res | Should -BeNullOrEmpty
+    }
 
-        It "Find not available module with wildcards" {
-            $res = Find-Module -Name "TestModule5test*" -Repository PSGallery
-            $res | Should -BeNullOrEmpty
-        }
-
-        It "Find-Module with min version" {
-            $res = Find-Module TestModule99 -MinimumVersion 0.0.3 -Repository PSGallery
-            $res.Name | Should -Be "TestModule99" 
-            $res.Version -ge [Version]"0.0.3" | Should -Be $true
-        }
-
-        It "Find-Module with min version not available" {
-            Find-Module TestModule99 -MinimumVersion 10.0 -Repository PSGallery
-            $res | Should -BeNullOrEmpty
-        }
-
-        It "Find-Module with required version not available" {
-            $res = Find-Module TestModule99 -RequiredVersion 12.0 -Repository PSGallery -ErrorVariable ev -ErrorAction SilentlyContinue
-            $res | Should -BeNullOrEmpty
-        }
-
-        It "Find-Module with required version" {
-            $res = Find-Module TestModule99 -RequiredVersion 0.0.2 -Repository PSGallery
-            $res | Should -Not -BeNullOrEmpty
-            $res.Name | Should -Be "TestModule99"
-            $res.Version -eq [Version]"0.0.2" | Should -Be $true
-        }
-
-        It "Find-Module with multiple module names and required version" {
-            $res = Find-Module TestModuleWithDependencyB, TestModuleWithDependencyC -RequiredVersion 3.0 -Repository PSGallery
-            $res.Count -eq 2 | Should -Be $true 
-            $res[0].Version -eq [Version]"3.0" | Should -Be $true
-            $res[1].Version -eq [Version]"3.0" | Should -Be $true
-        }
-
-        ### TODO:  Broken -- minimum version is broken
-<#
-        It "Find-Module with multiple module names and minimum version" {
-            $res = Find-Module TestModule99, TestModuleWithDependencyB -MinimumVersion 0.5 -Repository PSGallery
-            $res.Count -eq 2 | Should -Be $true 
-            $res[0].Version -gt [Version]"0.5" | Should -Be $true
-            $res[1].Version -gt [Version]"0.5" | Should -Be $true
-        }
-
-        
-        ### TODO:  broken
-        It "Find-Module with wildcard name and minimum version" {
-            $res = Find-Module TestModule9* -MinimumVersion 0.0.3 -Repository PSGallery -ErrorAction SilentlyContinue -ErrorVariable err
-            $err | Should -Not -BeNullOrEmpty
-            $res | Should -BeNullOrEmpty
-        }
-#>
-
-        It "Find-Module with multinames" {
-            $res = Find-Module TestModuleWithDependencyB, TestModuleWithDependencyC, TestModuleWithDependencyD -Repository PSGallery
-            $res.Count -eq 3 | Should -Be $true
-        }
-        
-        It "Find-Module with all versions" {
-            $res = Find-Module TestModule99 -Repository PSGallery -AllVersions
-            $res.Count -gt 1 | Should -Be $true
-        }
-
-        ### Broken
-<#
-        It "Find-DscResource with single resource name" {
-            $psgetDscResourceInfo = Find-DscResource -Name NetworkingDsc
-            $psgetDscResourceInfo.Name | Should -Be "NetworkingDsc" 
-        }
- 
-        ### Broken 
-        It "Find-DscResource with two resource names" {
-            $psgetDscResourceInfos = Find-DscResource -Name DscTestResource, NewDscTestResource
+    It "Find-Module with multinames" {
+        $res = Find-Module TestModuleWithDependencyB, TestModuleWithDependencyC, TestModuleWithDependencyD -Repository PSGallery
+        $res.Count -eq 3 | Should -Be $true
+    }
     
-            Assert ($psgetDscResourceInfos.Count -ge 2) "Find-DscResource did not return the expected DscResources, $psgetDscResourceInfos"
-    
-            Assert ($psgetDscResourceInfos.Name -contains "DscTestResource") "DscTestResource is not returned by Find-DscResource, $psgetDscResourceInfos"
-            Assert ($psgetDscResourceInfos.Name -contains "NewDscTestResource") "NewDscTestResource is not returned by Find-DscResource, $psgetDscResourceInfos"
-        }
-     
-        It "Find-Command with single command name" {
-            $psgetCommandInfo = Find-Command -Name Get-ContosoServer
-            AssertEquals $psgetCommandInfo.Name 'Get-ContosoServer' "Get-ContosoServer is not returned by Find-Command, $psgetCommandInfo"
-        }
+    It "Find-Module with all versions" {
+        $res = Find-Module TestModule99 -Repository PSGallery -AllVersions
+        $res.Count -gt 1 | Should -Be $true
+    }
 
-        It "FindCommandWithTwoResourceNames {
-            $psgetCommandInfos = Find-Command -Name Get-ContosoServer, Get-ContosoClient
-    
-            Assert ($psgetCommandInfos.Count -ge 2) "Find-Command did not return the expected command names, $psgetCommandInfos"
-    
-            Assert ($psgetCommandInfos.Name -contains 'Get-ContosoServer') "Get-ContosoServer is not returned by Find-Command, $psgetCommandInfos"
-            Assert ($psgetCommandInfos.Name -contains 'Get-ContosoClient') "Get-ContosoClient is not returned by Find-Command, $psgetCommandInfos"
+    It "Find-DscResource with single resource name" {
+        $res = Find-DscResource -Name SystemLocale -Repository $PSGalleryName
+        Foreach ($dscresource in $res) {
+            $dscresource.Names | Should -Be "SystemLocale"
+            $dscresource.ParentResource | Should -Be ("ComputerManagementDsc" -or "SystemLocaleDsc")
+        }   
+    }
+
+    It "Find-DscResource with two resource names" {
+        $res = Find-DscResource -Name "SystemLocale", "MSFT_PackageManagement" -Repository $PSGalleryName
+        Foreach ($dscresource in $res) {
+            $dscresource.Names | Should -Be ("SystemLocale" -or "MSFT_PackageManagement")
+            $dscresource.ParentResource | Should -Be ("ComputerManagementDsc" -or "SystemLocaleDsc" -or "PackageManagement")
         }
-#>       
-        It "Find-Module with IncludeDependencies" {
-            $ModuleName = "TestModuleWithDependencyE"
+    }
+  
+    It "Find-Command with single command name" {
+        $res = Find-Command -Name $commandName -Repository $PSGalleryName
+        $res | ForEach-Object { $_.Names | Should -Be $commandName }
+    }
+   
+    It "Find-Module with IncludeDependencies" {
+        $ModuleName = "TestModuleWithDependencyE"
     
-            $res = Find-Module -Name $ModuleName -IncludeDependencies
-            $res.Count -ge 2 | Should -Be $true
-        }
+        $res = Find-Module -Name $ModuleName -IncludeDependencies
+        $res.Count -ge 2 | Should -Be $true
+    }
 
     It "find module given specific Name, Version null" {
         $res = Find-Module -Name $testModuleName2 -Repository $PSGalleryName
@@ -204,25 +193,5 @@ Describe 'Test CompatPowerShellGet: Find-PSResource' -tags 'CI' {
         $resModuleType = Out-String -InputObject $resModule.Type
         $resModuleType.Replace(",", " ").Split() | Should -Contain "Module"
     }
-
-<#
-    ### BROKEN
-    It "find resource given CommandName" {
-        $res = Find-Command $commandName -Repository $PSGalleryName
-        foreach ($item in $res) {
-            $item.Names | Should -Be $commandName    
-            $item.ParentResource.Includes.Command | Should -Contain $commandName
-        }
-    }
-
-    ### BROKEN
-    It "find resource given DscResourceName" {
-        $res = Find-DscResource $dscResourceName -Repository $PSGalleryName
-        foreach ($item in $res) {
-            $item.Names | Should -Be $dscResourceName    
-            $item.ParentResource.Includes.DscResource | Should -Contain $dscResourceName
-        }
-    }
-#>
 }
     
--- test/GetPSResourceTests/CompatGetPSResource.Tests.ps1
+++ test/GetPSResourceTests/CompatGetPSResource.Tests.ps1
@@ -22,7 +22,7 @@ Describe 'Test CompatPowerShellGet: Get-PSResource' -Tags 'CI' {
         Install-PSResource -Name $testModuleName -Repository $PSGalleryName -Version "0.0.1"
         Install-PSResource -Name $testModuleName -Repository $PSGalleryName -Version "0.0.2"
         Install-PSResource -Name $testModuleName -Repository $PSGalleryName -Version "0.0.3"
-        Install-PSResource -Name $testScriptName -Repository $PSGalleryName
+        Install-PSResource -Name $testScriptName -Repository $PSGalleryName -SkipDependencyCheck
     }
 
     AfterAll {
@@ -32,43 +32,50 @@ Describe 'Test CompatPowerShellGet: Get-PSResource' -Tags 'CI' {
     }
 
     It "Get-InstalledModule with MinimumVersion available" {        
-        $module = Get-InstalledModule -Name $testModuleName -MinimumVersion "0.0.1"
-        $module.Count -ge 1 | Should -Be $true        
+        $res = Get-InstalledModule -Name $testModuleName -MinimumVersion "0.0.1"
+        $res.Count -ge 1 | Should -Be $true   
+        foreach ($pkg in $res)
+        {
+            $pkg.Version -ge [System.Version] "0.0.1" | Should -Be $true
+        }
     }
 
     It "Get-InstalledScript with MinimumVersion available" {        
-        $module = Get-InstalledScript -Name $testModuleName -MinimumVersion "1.0.0"
-        $module.Count -ge 1 | Should -Be $true        
+        $res = Get-InstalledScript -Name $testScriptName -MinimumVersion "1.0.0"
+        $res.Count -ge 1 | Should -Be $true    
+        foreach ($pkg in $res)
+        {
+            $pkg.Version -ge [System.Version] "1.0.0" | Should -Be $true
+        }    
     }
 
-    ### Broken
-<#
     It "Get-InstalledModule with MinimumVersion not available" {        
-        $module = Get-InstalledModule -Name $testModuleName -MinimumVersion "0.0.4"
-        $module.Count -eq 0 | Should -Be $true     
+        $res = Get-InstalledModule -Name $testModuleName -MinimumVersion "1.0.0"
+        $res.Count -eq 0 | Should -Be $true        
     }
 
-    ### broken
     It "Get-InstalledModule with min/max range" {
-        $module = Get-InstalledModule -Name $testModuleName -MinimumVersion "0.0.15" -MaximumVersion "0.0.25"
-        $module.Version | Should -Be "0.0.2"        
+        $res = Get-InstalledModule -Name $testModuleName -MinimumVersion "0.0.15" -MaximumVersion "0.0.25" 
+        foreach ($pkg in $res)
+        {
+            $pkg.Version -ge [System.Version] "0.0.2" | Should -Be $true
+        }       
     }
 
-    ### broken
     It "Get-InstalledModule with -RequiredVersion" {
         $version = "0.0.2"
-        $module = Get-InstalledModule -Name $testModuleName -RequiredVersion $version
-        $module.Version | Should -Be $version    
+        $res = Get-InstalledModule -Name $testModuleName -RequiredVersion $version
+        $res.Version | Should -Be $version    
     }
     
     It "Get prerelease version module when version with correct prerelease label is specified" {
-        Install-PSResource -Name $testModuleName -Version "5.2.5-alpha001" -Repository $PSGalleryName -TrustRepository
-        $res = Get-PSResource -Name $testModuleName -Version "5.2.5"
+        Install-PSResource -Name $testModuleName -Version "1.0.0-beta2" -Repository $PSGalleryName
+        $res = Get-PSResource -Name $testModuleName -Version "1.0.0"
         $res | Should -BeNullOrEmpty
-        $res = Get-PSResource -Name $testModuleName -Version "5.2.5-alpha001"
+        $res = Get-PSResource -Name $testModuleName -Version "1.0.0-beta2"
         $res.Name | Should -Be $testModuleName
-        $res.Version | Should -Be "5.2.5"
-        $res.Prerelease | Should -Be "alpha001"
+        $res.Version | Should -Be "1.0.0"
+        $res.Prerelease | Should -Be "beta2"
     }
 
     It "Get prerelease version script when version with correct prerelease label is specified" {
@@ -80,9 +87,6 @@ Describe 'Test CompatPowerShellGet: Get-PSResource' -Tags 'CI' {
         $res.Version | Should -Be "3.0.0"
         $res.Prerelease | Should -Be "alpha"
     }
-
-
-#>
 
     It "Get-InstalledModule with Wildcard" {
         $module = Get-InstalledModule -Name "testmodule9*"
@@ -113,17 +117,20 @@ Describe 'Test CompatPowerShellGet: Get-PSResource' -Tags 'CI' {
         $pkg = Get-InstalledScript -Name $testScriptName
         $pkg.Name | Should -Be $testScriptName
     }
-<#
-    # BROKEN - compat issue 
-    It "Get resource when given Name to <Reason> <Version>" -TestCases @(
-        @{Name="*est_modul*";    ExpectedName=$testModuleName; Reason="validate name, with wildcard at beginning and end of name: *est_modul*"}
-      #  @{Name="test_mod*";      ExpectedName=$testModuleName; Reason="validate name, with wildcard at end of name: test_mod*"},
-      #  @{Name="*est_module";    ExpectedName=$testModuleName; Reason="validate name, with wildcard at beginning of name: *est_module"},
-      #  @{Name="tes*ule";        ExpectedName=$testModuleName; Reason="validate name, with wildcard in middle of name: tes*ule"}
+
+    It "Get resource when given Name to " {
+        $pkgs = Get-InstalledModule -Name "*estmodul*"
+        $pkgs.Name | Should -Contain $testModuleName
+    }
+
+    It "Get resource when given Name to <Reason>" -TestCases @(
+        @{Name="*estmodul*";    Reason="validate name, with wildcard at beginning and end of name: *estmodul*"},
+        @{Name="testmod*";      Reason="validate name, with wildcard at end of name: testmod*"},
+        @{Name="*estmodule99";  Reason="validate name, with wildcard at beginning of name: *estmodule99"},
+        @{Name="tes*ule99";     Reason="validate name, with wildcard in middle of name: tes*ule99"}
     ) {
-        param($Version, $ExpectedVersion)
+        param($Name)
         $pkgs = Get-InstalledModule -Name $Name
         $pkgs.Name | Should -Contain $testModuleName
     }
-#>
 }
