a8265f96a0a43a3ebafbb9ca7501c538e7c04d6e
alerickson <25858831+alerickson@users.noreply.github.com>
2023-04-20 12:02:49 -0700

    remove get from ci test

--- test/GetPSResourceTests/CompatGetPSResource.Tests.ps1
+++ test/GetPSResourceTests/CompatGetPSResource.Tests.ps1
@@ -9,7 +9,7 @@ Import-Module $modPath -Force -Verbose
 $buildModule = "$psscriptroot/../../out/PowerShellGet"
 Import-Module $buildModule -Force -Verbose
 
-Describe 'Test CompatPowerShellGet: Get-PSResource' -tags 'CI' {
+Describe 'Test CompatPowerShellGet: Get-PSResource' {
 
     BeforeAll{
         $PSGalleryName = Get-PSGalleryName
