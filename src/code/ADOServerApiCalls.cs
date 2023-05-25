c51b9261cb7d138f9c8330a2d2325bad246a74f1
Anam Navied <anam.naviyou@gmail.com>
2023-05-25 01:11:36 -0400

    code clean up and add more comments

--- src/code/ADOServerApiCalls.cs
+++ src/code/ADOServerApiCalls.cs
@@ -27,6 +27,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
         private bool _isNuGetRepo { get; set; }
         public FindResponseType v3FindResponseType = FindResponseType.ResponseString;
         private static readonly Hashtable[] emptyHashResponses = new Hashtable[]{};
+        private static readonly string nugetRepoUri = "https://api.nuget.org/v3/index.json";
         private static readonly string resourcesName = "resources";
         private static readonly string itemsName = "items";
         private static readonly string countName = "count";
@@ -53,7 +54,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
 
             _sessionClient = new HttpClient(handler);
 
-            _isNuGetRepo = String.Equals(Repository.Uri.AbsoluteUri, "https://api.nuget.org/v3/index.json", StringComparison.InvariantCultureIgnoreCase);
+            _isNuGetRepo = String.Equals(Repository.Uri.AbsoluteUri, nugetRepoUri, StringComparison.InvariantCultureIgnoreCase);
         }
 
         #endregion
@@ -66,7 +67,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
         /// </summary>
         public override FindResults FindAll(bool includePrerelease, ResourceType type, out ExceptionDispatchInfo edi)
         {
-            string errMsg = $"Find all is not supported for the repository {Repository.Uri}";
+            string errMsg = $"Find all is not supported for the V3 repository {Repository.Name}";
             edi = ExceptionDispatchInfo.Capture(new InvalidOperationException(errMsg));
 
             return new FindResults(stringResponse: Utils.EmptyStrArray, hashtableResponse: emptyHashResponses, responseType: v3FindResponseType);
@@ -74,7 +75,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
 
         /// <summary>
         /// Find method which allows for searching for packages with tag(s) from a repository and returns latest version for each.
-        /// Not supported for ADO repository.
+        /// This is supported only for the NuGet repository special case, not other V3 repositories.
         /// </summary>
         public override FindResults FindTags(string[] tags, bool includePrerelease, ResourceType type, out ExceptionDispatchInfo edi)
         {
@@ -84,7 +85,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
             }
             else
             {
-                string errMsg = $"Find by Tags is not supported for the repository {Repository.Uri}";
+                string errMsg = $"Find by Tags is not supported for the V3 repository {Repository.Name}";
                 edi = ExceptionDispatchInfo.Capture(new InvalidOperationException(errMsg));
                 return new FindResults(stringResponse: Utils.EmptyStrArray, hashtableResponse: emptyHashResponses, responseType: v3FindResponseType);
             }
@@ -96,7 +97,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
         /// </summary>
         public override FindResults FindCommandOrDscResource(string[] tags, bool includePrerelease, bool isSearchingForCommands, out ExceptionDispatchInfo edi)
         {
-            string errMsg = $"Find by CommandName or DSCResource is not supported for {Repository.Name} as it uses the V3 server protocol";
+            string errMsg = $"Find by CommandName or DSCResource is not supported for the V3 server repository {Repository.Name}";
             edi = ExceptionDispatchInfo.Capture(new InvalidOperationException(errMsg));
 
             return new FindResults(stringResponse: Utils.EmptyStrArray, hashtableResponse: emptyHashResponses, responseType: v3FindResponseType);
@@ -126,7 +127,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
 
         /// <summary>
         /// Find method which allows for searching for single name with wildcards and returns latest version.
-        /// This is not supported for ADO repository.
+        /// This is supported only for the NuGet repository special case, not other V3 repositories.
         /// </summary>
         public override FindResults FindNameGlobbing(string packageName, bool includePrerelease, ResourceType type, out ExceptionDispatchInfo edi)
         {
@@ -136,7 +137,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
             }
             else
             {
-                string errMsg = $"Find with Name containing wildcards is not supported for the repository {Repository.Name}";
+                string errMsg = $"Find with Name containing wildcards is not supported for the V3 server repository {Repository.Name}";
                 edi = ExceptionDispatchInfo.Capture(new InvalidOperationException(errMsg));
 
                 return new FindResults(stringResponse: Utils.EmptyStrArray, hashtableResponse: emptyHashResponses, responseType: v3FindResponseType);
@@ -145,7 +146,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
 
         /// <summary>
         /// Find method which allows for searching for single name with wildcards and tag and returns latest version.
-        /// This is not supported for ADO repository.
+        /// This is supported only for the NuGet repository special case, not other V3 repositories.
         /// </summary>
         public override FindResults FindNameGlobbingWithTag(string packageName, string[] tags, bool includePrerelease, ResourceType type, out ExceptionDispatchInfo edi)
         {
@@ -155,7 +156,7 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
             }
             else
             {
-                string errMsg = $"Find with Name containing wildcards is not supported for the repository {Repository.Name}";
+                string errMsg = $"Find with Name containing wildcards is not supported for the V3 server repository {Repository.Name}";
                 edi = ExceptionDispatchInfo.Capture(new InvalidOperationException(errMsg));
 
                 return new FindResults(stringResponse: Utils.EmptyStrArray, hashtableResponse: emptyHashResponses, responseType: v3FindResponseType);
@@ -371,6 +372,9 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
 
         #region Private Methods
 
+        /// <summary>
+        /// Helper method called by FindNameGlobbing() and FindNameGlobbingWithTag() for special case where repository is NuGet.org repository.
+        /// </summary>
         private FindResults FindNameGlobbingFromNuGetRepo(string packageName, string[] tags, bool includePrerelease, out ExceptionDispatchInfo edi)
         {
             var names = packageName.Split(new char[] { '*' }, StringSplitOptions.RemoveEmptyEntries);
@@ -472,7 +476,10 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
 
             return new FindResults(stringResponse: matchingResponses.ToArray(), hashtableResponse: emptyHashResponses, responseType: v3FindResponseType);
         }
-        
+
+        /// <summary>
+        /// Helper method called by FindTags() for special case where repository is NuGet.org repository.
+        /// </summary>        
         private FindResults FindTagsFromNuGetRepo(string[] tags, bool includePrerelease, out ExceptionDispatchInfo edi)
         {
             Dictionary<string, string> resources = GetResourcesFromServiceIndex(out edi);
@@ -492,53 +499,18 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
             string query = $"{searchQueryServiceUrl}?q=tags:{tagsQueryTerm}&prerelease={includePrerelease}&semVerLevel=2.0.0";
             Console.WriteLine($"tag query: {query}");
 
-            // Get responses for all packages (only their latest, prerelease satisfying version) that includes the tag (somewhere in the metadata)
+            // Get responses for all packages that contain the required tags
             JsonElement[] tagPkgs = GetJsonElementArr(query, dataName, out edi);
             if (edi != null)
             {
                 return new FindResults(stringResponse: Utils.EmptyStrArray, hashtableResponse: emptyHashResponses, responseType: v3FindResponseType);
             }
 
-            // loop through responses returned
-            // get tags really, and if it satisfies it all, add to list and return. ResponseUtil should handle rest
-
             List<string> matchingResponses = new List<string>();
             foreach (var pkgEntry in tagPkgs)
             {
                 try
                 {
-                    // Since the query only matched the tag value to be present in the metadata, not the actual "tag" property
-                    // if (!pkgId.TryGetProperty(idName, out JsonElement idItem) || !pkgId.TryGetProperty(versionName, out JsonElement versionItem))
-                    // {
-                    //     string errMsg = $"FindTag(): Id or Version element could not be found in response.";
-                    //     edi = ExceptionDispatchInfo.Capture(new JsonParsingException(errMsg));
-                    //     return new FindResults(stringResponse: Utils.EmptyStrArray, hashtableResponse: emptyHashResponses, responseType: v3FindResponseType);
-                    // }
-
-                    // RECENT
-                    // if (!pkgEntry.TryGetProperty("tags", out JsonElement tagsItem))
-                    // {
-                    //     string errMsg = $"FindTag(): Tag element could not be found in response.";
-                    //     edi = ExceptionDispatchInfo.Capture(new JsonParsingException(errMsg));
-                    //     return new FindResults(stringResponse: Utils.EmptyStrArray, hashtableResponse: emptyHashResponses, responseType: v3FindResponseType);
-                    // }
-
-                    // string[] pkgTags = GetTagsFromJsonElement(tagsItem);
-                    // bool isTagMatch = true;
-                    // foreach (string rqTag in tags)
-                    // {
-                    //     if (!pkgTags.Contains(rqTag, StringComparer.OrdinalIgnoreCase))
-                    //     {
-                    //         isTagMatch = false;
-                    //         break;
-                    //     }
-                    // }
-
-                    // if (!isTagMatch)
-                    // {
-                    //     continue;
-                    // }
-
                     matchingResponses.Add(pkgEntry.ToString());
                 }
                 catch (Exception e)
@@ -601,6 +573,8 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
                     {
                         // versions are reported in descending order i.e 5.0.0, 3.0.0, 1.0.0 so grabbing the first match suffices
                         latestVersionResponse = response;
+
+                        // TODO: technically we could call GetTagsFromJsonElement() and DeterminePkgTagsSatisfyRequiredTags() so we don't parse into JsonDocument again
                         break;
                     }
                 } 
@@ -684,6 +658,8 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
                     if (pkgVersion == requiredVersion)
                     {
                         latestVersionResponse = response;
+                        // TODO: technically we could call GetTagsFromJsonElement() and DeterminePkgTagsSatisfyRequiredTags() so we don't parse into JsonDocument again
+                        // regardless of wheter tags are satisfied we would break - if false that just means we had right version but no tags, so search fails and edi is set but response is empty (handled below)
                         break;
                     }
                 }
@@ -774,6 +750,9 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
             return content;
         }
 
+        /// <summary>
+        /// Finds all resources present in the repository's service index.
+        /// </summary>
         private Dictionary<string, string> GetResourcesFromServiceIndex(out ExceptionDispatchInfo edi)
         {
             Dictionary<string, string> resources = new Dictionary<string, string>();
@@ -789,12 +768,14 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
                 {
                     if (!resource.TryGetProperty("@type", out JsonElement typeElement))
                     {
-                        // TODO: some error
+                        edi = ExceptionDispatchInfo.Capture(new JsonParsingException($"@type element not found for resource in service index for repository {Repository.Name}"));
+                        return new Dictionary<string, string>();
                     }
 
                     if (!resource.TryGetProperty("@id", out JsonElement idElement))
                     {
-                        // TODO: some error
+                        edi = ExceptionDispatchInfo.Capture(new JsonParsingException($"@id element not found for resource in service index for repository {Repository.Name}"));
+                        return new Dictionary<string, string>();
                     }
 
                     if (!resources.ContainsKey(typeElement.ToString()))
@@ -802,7 +783,6 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
                         // Some resources have a primary and secondary entry. The @id value is the same, so we only choose the primary entry.
                         resources.Add(typeElement.ToString(), idElement.ToString());
                     }
-
                 }
                 catch (Exception e)
                 {
@@ -815,6 +795,10 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
             return resources;
         }
         
+        /// <summary>
+        /// Gets the resource of type "RegistrationBaseUrl" from the repository's resources.
+        /// A repository can have multiple resources of type "RegistrationsBaseUrl" so it finds the best match according to the guideline comment in the method.
+        /// </summary>
         private string FindRegistrationsBaseUrlForAllV3(Dictionary<string, string> resources, out ExceptionDispatchInfo edi)
         {
             edi = null;
@@ -857,6 +841,10 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
             return registrationsBaseUrl;
         }
 
+        /// <summary>
+        /// Gets the resource of type "SearchQueryService" from the repository's resources.
+        /// A repository can have multiple resources of type "SearchQueryService" so it finds the best match according to the guideline comment in the method.
+        /// </summary>
         private string FindSearchQueryService(Dictionary<string, string> resources, out ExceptionDispatchInfo edi)
         {
             edi = null;
@@ -884,37 +872,6 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
             }
 
             return searchQueryServiceUrl;
-        }
-
-        private bool IsLatestVersionFirst(string[] versionedResponses, string upperVersion, out ExceptionDispatchInfo edi)
-        {
-            edi = null;
-            bool latestVersionFirst = true;
-            // We don't need to perform this check if no responses, or single response
-            if (versionedResponses.Length < 2)
-            {
-                return latestVersionFirst;
-            }
-
-            string firstResponse = versionedResponses[0];
-            JsonDocument firstResponseJson = JsonDocument.Parse(firstResponse);
-            JsonElement firstResponseDom = firstResponseJson.RootElement;
-            if (!firstResponseDom.TryGetProperty(versionName, out JsonElement firstVersionElement))
-            {
-                edi = ExceptionDispatchInfo.Capture(new JsonParsingException("Response did not contain 'version' element"));
-                return latestVersionFirst;
-            }
-
-            string firstVersion = firstVersionElement.ToString();
-            if (NuGetVersion.TryParse(upperVersion, out NuGetVersion upperPkgVersion) && NuGetVersion.TryParse(firstVersion, out NuGetVersion firstPkgVersion))
-            {
-                if (firstPkgVersion != upperPkgVersion)
-                {
-                    latestVersionFirst = false;
-                }
-            }
-
-            return latestVersionFirst;
         }
 
         /// <summary>
@@ -1003,6 +960,42 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
             }
 
             return versionedResponseArr;
+        }
+
+        /// <summary>
+        /// Returns true if the metadata entries are arranged in descending order with respect to the package's version.
+        /// ADO feeds usually return version entries in descending order, but Nuget.org repository returns them in ascending order.
+        /// </summary>
+        private bool IsLatestVersionFirst(string[] versionedResponses, string upperVersion, out ExceptionDispatchInfo edi)
+        {
+            edi = null;
+            bool latestVersionFirst = true;
+
+            // We don't need to perform this check if no responses, or single response
+            if (versionedResponses.Length < 2)
+            {
+                return latestVersionFirst;
+            }
+
+            string firstResponse = versionedResponses[0];
+            JsonDocument firstResponseJson = JsonDocument.Parse(firstResponse);
+            JsonElement firstResponseDom = firstResponseJson.RootElement;
+            if (!firstResponseDom.TryGetProperty(versionName, out JsonElement firstVersionElement))
+            {
+                edi = ExceptionDispatchInfo.Capture(new JsonParsingException($"Response did not contain '{versionName}' element"));
+                return latestVersionFirst;
+            }
+
+            string firstVersion = firstVersionElement.ToString();
+            if (NuGetVersion.TryParse(upperVersion, out NuGetVersion upperPkgVersion) && NuGetVersion.TryParse(firstVersion, out NuGetVersion firstPkgVersion))
+            {
+                if (firstPkgVersion != upperPkgVersion)
+                {
+                    latestVersionFirst = false;
+                }
+            }
+
+            return latestVersionFirst;
         }
 
         /// <summary>
