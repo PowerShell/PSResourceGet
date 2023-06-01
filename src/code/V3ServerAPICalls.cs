4defe2df7f83f07afe7e9d66876c37a027e3797f
alerickson <25858831+alerickson@users.noreply.github.com>
2023-06-01 16:12:01 -0700

    comment out section

--- src/code/V3ServerAPICalls.cs
+++ src/code/V3ServerAPICalls.cs
@@ -847,6 +847,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                             return Utils.EmptyStrArray;
                         }
                         
+                        /*
                         // If metadata has a "listed" property, but it's set to false, skip this package version
                         if (metadataElement.TryGetProperty("listed", out JsonElement listedElement))
                         {
@@ -855,6 +856,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                                 continue;
                             }
                         }
+                        */
                         
                         versionedResponses.Add(metadataElement.ToString());
                     }
