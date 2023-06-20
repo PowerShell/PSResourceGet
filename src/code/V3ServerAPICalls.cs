8051cab46134d1518238466ff64f258da5b2c878
Anam Navied <anam.naviyou@gmail.com>
2023-06-20 15:05:48 -0400

    add support for MyGet for FindNameGlobbing scenarios

--- src/code/V3ServerAPICalls.cs
+++ src/code/V3ServerAPICalls.cs
@@ -25,6 +25,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
         private bool _isNuGetRepo { get; set; }
         private bool _isJFrogRepo { get; set; }
         private bool _isGHPkgsRepo { get; set; }
+        private bool _isMyGetRepo { get; set; }
         public FindResponseType v3FindResponseType = FindResponseType.ResponseString;
         private static readonly Hashtable[] emptyHashResponses = new Hashtable[]{};
         private static readonly string nugetRepoUri = "https://api.nuget.org/v3/index.json";
@@ -58,6 +59,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
             _isNuGetRepo = String.Equals(Repository.Uri.AbsoluteUri, nugetRepoUri, StringComparison.InvariantCultureIgnoreCase);
             _isJFrogRepo = Repository.Uri.AbsoluteUri.ToLower().Contains("jfrog.io");
             _isGHPkgsRepo = Repository.Uri.AbsoluteUri.ToLower().Contains("pkg.github.com");
+            _isMyGetRepo = Repository.Uri.AbsoluteUri.ToLower().Contains("myget.org");
         }
 
         #endregion
@@ -134,7 +136,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
         /// </summary>
         public override FindResults FindNameGlobbing(string packageName, bool includePrerelease, ResourceType type, out ErrorRecord errRecord)
         {
-            if (_isNuGetRepo || _isJFrogRepo || _isGHPkgsRepo)
+            if (_isNuGetRepo || _isJFrogRepo || _isGHPkgsRepo || _isMyGetRepo)
             {
                 return FindNameGlobbingFromNuGetRepo(packageName, tags: Utils.EmptyStrArray, includePrerelease, out errRecord);
             }
@@ -153,7 +155,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
         /// </summary>
         public override FindResults FindNameGlobbingWithTag(string packageName, string[] tags, bool includePrerelease, ResourceType type, out ErrorRecord errRecord)
         {
-            if (_isNuGetRepo || _isJFrogRepo || _isGHPkgsRepo)
+            if (_isNuGetRepo || _isJFrogRepo || _isGHPkgsRepo || _isMyGetRepo)
             {
                 return FindNameGlobbingFromNuGetRepo(packageName, tags, includePrerelease, out errRecord);
             }
@@ -1107,11 +1109,12 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                         responseEntries.Add(entry.Clone());
                     }
 
-                    totalHits = 0;
-                    if (pkgsDom.RootElement.TryGetProperty("totalHits", out JsonElement totalHitsElement))
-                    {
-                        int.TryParse(totalHitsElement.ToString(), out totalHits);
-                    }
+                    // totalHits = 0;
+                    totalHits = entryElement.GetArrayLength();
+                    // if (pkgsDom.RootElement.TryGetProperty("totalHits", out JsonElement totalHitsElement))
+                    // {
+                    //     int.TryParse(totalHitsElement.ToString(), out totalHits);
+                    // }
 
                     entries = responseEntries.ToArray();
                 }
