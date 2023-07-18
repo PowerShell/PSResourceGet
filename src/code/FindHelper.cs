36839ccf9382508866dd500cbceb118a98db203c
Anam Navied <anam.naviyou@gmail.com>
2023-07-18 14:32:43 -0400

    Fix install for NuGet.Server feed (#1253)

--- src/code/FindHelper.cs
+++ src/code/FindHelper.cs
@@ -746,7 +746,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                         if (responses.IsFindResultsEmpty())
                         {
                             _cmdletPassedIn.WriteError(new ErrorRecord(
-                                        new InvalidOrEmptyResponse($"Dependency package with Name {dep.Name} and VersionRange {dep.VersionRange} could not be found"), 
+                                        new InvalidOrEmptyResponse($"Dependency package with Name {dep.Name} and VersionRange {dep.VersionRange} could not be found in repository '{repository.Name}"), 
                                         "FindDepPackagesFindVersionGlobbingFailure", 
                                         ErrorCategory.InvalidResult, 
                                         this));
@@ -759,7 +759,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                             if (currentResult.exception != null && !currentResult.exception.Message.Equals(string.Empty))
                             {
                                  _cmdletPassedIn.WriteError(new ErrorRecord(
-                                            new PackageNotFoundException($"Dependency package '{dep.Name}' with version range '{dep.VersionRange}' could not be found", currentResult.exception), 
+                                            new PackageNotFoundException($"Dependency package '{dep.Name}' with version range '{dep.VersionRange}' could not be found in repository '{repository.Name}'", currentResult.exception), 
                                             "DependencyPackageNotFound", 
                                             ErrorCategory.ObjectNotFound, 
                                             this));
@@ -784,13 +784,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
 
                         if (depPkg == null)
                         {
-                            _cmdletPassedIn.WriteError(new ErrorRecord(
-                                            new PackageNotFoundException($"Dependency package '{dep.Name}' with version range '{dep.VersionRange}' could not be found"), 
-                                            "DependencyPackageNotFound", 
-                                            ErrorCategory.ObjectNotFound, 
-                                            this));
-                                
-                            yield return null;
+                            continue;
                         }
                         
                         string pkgHashKey = String.Format("{0}{1}", depPkg.Name, depPkg.Version.ToString());
