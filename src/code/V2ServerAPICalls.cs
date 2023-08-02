1ecd9e2a853aec4d5f5d3fd27509324acfbfdd2e
alerickson <25858831+alerickson@users.noreply.github.com>
2023-08-02 15:24:05 -0700

    update bug found in tests

--- src/code/V2ServerAPICalls.cs
+++ src/code/V2ServerAPICalls.cs
@@ -931,8 +931,13 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                 // ADO feed will return a 401 if a package does not exist on the feed, with the following message:
                 // 401 (Unauthorized - No local versions of package 'NonExistentModule'; please provide authentication to access
                 // versions from upstream that have not yet been saved to your feed. (DevOps Activity ID: 5E5CF528-5B3D-481D-95B5-5DDB5476D7EF))
-                if (responseStatusCode.Equals(HttpStatusCode.Unauthorized) && !e.Message.Contains("access versions from upstream that have not yet been saved to your feed"))
+                if (responseStatusCode.Equals(HttpStatusCode.Unauthorized))
                 {
+                    if (e.Message.Contains("access versions from upstream that have not yet been saved to your feed"))
+                    {
+                        throw new ResourceNotFoundException(Utils.FormatRequestsExceptions(e, message));
+                    }
+
                     throw new UnauthorizedException(Utils.FormatCredentialRequestExceptions(e));
                 }
 
