a24cca5a0548fa8eb2710b27ad463f4cad21cf66
alerickson <25858831+alerickson@users.noreply.github.com>
2023-08-09 12:23:10 -0700

    Bug fix for saving Az dependencies (#1343)

--- src/code/V2ServerAPICalls.cs
+++ src/code/V2ServerAPICalls.cs
@@ -839,10 +839,14 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
             if (versionRange.MaxVersion != null)
             {
                 string operation = versionRange.IsMaxInclusive ? "le" : "lt";
-                // Adding 1 because we want to retrieve all the prerelease versions for the max version and PSGallery views prerelease as higher than its stable
+                // Adding '9' as a digit to the end of the patch portion of the version
+                // because we want to retrieve all the prerelease versions for the upper end of the range
+                // and PSGallery views prerelease as higher than its stable.
                 // eg 3.0.0-prerelease > 3.0.0
-                string maxString = includePrerelease ? $"{versionRange.MaxVersion.Major}.{versionRange.MaxVersion.Minor + 1}" :
-                                $"{versionRange.MaxVersion.ToNormalizedString()}";
+                // If looking for versions within '[1.9.9,1.9.9]' including prerelease values, this will change it to search for '[1.9.9,1.9.99]' 
+                // and find any pkg versions that are 1.9.9-prerelease.
+                string maxString = includePrerelease ? $"{versionRange.MaxVersion.Major}.{versionRange.MaxVersion.Minor}.{versionRange.MaxVersion.Patch.ToString() + "9"}" :
+                                 $"{versionRange.MaxVersion.ToNormalizedString()}";
                 if (NuGetVersion.TryParse(maxString, out NuGetVersion maxVersion))
                 {
                     maxPart = String.Format(format, operation, $"'{maxVersion.ToNormalizedString()}'");
