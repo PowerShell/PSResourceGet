6d06aed696c174793d2fd688175919acc9c69eab
Anam Navied <anam.naviyou@gmail.com>
2023-09-05 12:52:28 -0400

    Update src/code/Utils.cs
    Co-authored-by: alerickson <25858831+alerickson@users.noreply.github.com>

--- src/code/Utils.cs
+++ src/code/Utils.cs
@@ -739,7 +739,7 @@ namespace Microsoft.PowerShell.PSResourceGet.UtilClasses
                 cmdletPassedIn.ThrowTerminatingError(
                     new ErrorRecord(
                         new PSInvalidOperationException(
-                            message: $"Cannot validate Microsoft.PowerShell.SecretManagement module setup for PSResourceRepository ({repositoryName}) authentication.",
+                            message: $"Cannot validate Microsoft.PowerShell.SecretManagement module setup for PSResourceRepository '{repositoryName}' authentication.",
                             innerException: e),
                         "RepositoryCredentialSecretManagementInvalidModule",
                         ErrorCategory.InvalidOperation,
