d1687f1d761efc69486407bc38dbffa339803395
Anam Navied <anam.naviyou@gmail.com>
2023-09-25 13:11:02 -0400

    add TrustRepository

--- buildtools.psm1
+++ buildtools.psm1
@@ -120,7 +120,7 @@ function Install-ModulePackageForTest {
     }
 
     Write-Verbose -Verbose -Message "Installing module $($config.ModuleName) to build output path $installationPath"
-    Save-PSResource -Name $config.ModuleName -Repository $localRepoName -Path $installationPath -SkipDependencyCheck -Prerelease -Confirm:$false
+    Save-PSResource -Name $config.ModuleName -Repository $localRepoName -Path $installationPath -SkipDependencyCheck -Prerelease -Confirm:$false -TrustRepository
 
     Write-Verbose -Verbose -Message "Unregistering local package repo: $localRepoName"
     Unregister-PSResourceRepository -Name $localRepoName -Confirm:$false
