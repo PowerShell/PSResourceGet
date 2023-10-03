6676d1c63152637d2052015e4ceb04f4e693c68b
Anam Navied <anam.naviyou@gmail.com>
2023-10-03 12:21:08 -0400

    Update src/code/InstallPSResource.cs
    Co-authored-by: alerickson <25858831+alerickson@users.noreply.github.com>

--- src/code/InstallPSResource.cs
+++ src/code/InstallPSResource.cs
@@ -440,7 +440,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                 {
                     var requiredResourceHashtableInputFormatError = new ErrorRecord(
                         new ArgumentException($"The RequiredResource input with name {pkgName} does not have a valid value, the value must be a hashtable."),
-                        "RequiredReourceHashtableInputFormatError", 
+                        "RequiredResourceHashtableInputFormatError", 
                         ErrorCategory.InvalidArgument,
                         this);
 
