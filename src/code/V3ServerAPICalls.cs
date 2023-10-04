8554746f27698d414a54ded704d99018e58a2ff6
Anam Navied <anam.naviyou@gmail.com>
2023-10-04 11:46:09 -0400

    catch specific exception

--- src/code/V3ServerAPICalls.cs
+++ src/code/V3ServerAPICalls.cs
@@ -1476,10 +1476,10 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
             {
                 // scenario where the feed is not active anymore, i.e confirmed for JFrogArtifactory. The default error message is not intuitive.
                 errRecord = new ErrorRecord(
-                exception: new Exception($"JSON response from repository {Repository.Name} could not be parsed, likely due to the feed being inactive or invalid, with inner exception: {e.Message}"), 
-                "FindVersionGlobbingFailure",
-                ErrorCategory.InvalidResult, 
-                this);
+                    exception: new Exception($"JSON response from repository {Repository.Name} could not be parsed, likely due to the feed being inactive or invalid, with inner exception: {e.Message}"), 
+                    "FindVersionGlobbingFailure",
+                    ErrorCategory.InvalidResult, 
+                    this);
             }
             catch (Exception e)
             {
