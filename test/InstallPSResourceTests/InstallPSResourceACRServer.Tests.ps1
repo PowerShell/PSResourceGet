136fb95a3f6c6cf49ec9aa8890a91c6d148383c7
alerickson <25858831+alerickson@users.noreply.github.com>
2024-02-14 11:21:09 -0800

    Add ACR Install tests (#1560)

--- test/InstallPSResourceTests/InstallPSResourceACRServer.Tests.ps1
+++ test/InstallPSResourceTests/InstallPSResourceACRServer.Tests.ps1
@@ -5,38 +5,34 @@ $ProgressPreference = "SilentlyContinue"
 $modPath = "$psscriptroot/../PSGetTestUtils.psm1"
 Import-Module $modPath -Force -Verbose
 
-Describe 'Test Install-PSResource for GitHub packages' -tags 'CI' {
+Describe 'Test Install-PSResource for ACR scenarios' -tags 'CI' {
 
     BeforeAll {
-        $testModuleName = "test_module"
-        $testModuleName2 = "test_module2"
-        $testScriptName = "test_script"
-        $GithubPackagesRepoName = "GithubPackagesRepo"
-        $GithubPackagesRepoUri = "https://nuget.pkg.github.com/PowerShell/index.json"
+        $testModuleName = "test_local_mod"
+        $testModuleName2 = "test_local_mod2"
+        $testScriptName = "test_ado_script"
+        $ACRRepoName = "ACRRepo"
+        $ACRRepoUri = "https://psresourcegettest.azurecr.io/"
         Get-NewPSResourceRepositoryFile
-        Register-PSResourceRepository -Name $GithubPackagesRepoName -Uri $GithubPackagesRepoUri
-
-        $secureString = ConvertTo-SecureString $env:MAPPED_GITHUB_PAT -AsPlainText -Force
-        $credential = New-Object pscredential ($env:GITHUB_USERNAME, $secureString)
-
-        Uninstall-PSResource $testModuleName, $testScriptName -Version "*" -SkipDependencyCheck -ErrorAction SilentlyContinue
+        $psCredInfo = New-Object Microsoft.PowerShell.PSResourceGet.UtilClasses.PSCredentialInfo ("SecretStore", "$env:TENANTID")
+        Register-PSResourceRepository -Name $ACRRepoName -ApiVersion 'acr' -Uri $ACRRepoUri -CredentialInfo $psCredInfo -Verbose
     }
 
     AfterEach {
-        Uninstall-PSResource $testModuleName, $testScriptName -Version "*" -SkipDependencyCheck -ErrorAction SilentlyContinue
+        Uninstall-PSResource $testModuleName, $testModuleName2, $testScriptName -Version "*" -SkipDependencyCheck -ErrorAction SilentlyContinue
     }
 
     AfterAll {
         Get-RevertPSResourceRepositoryFile
     }
 
-    $testCases = @{Name="*";                          ErrorId="NameContainsWildcard"},
-                 @{Name="Test_m*";                    ErrorId="NameContainsWildcard"},
-                 @{Name="Test?module","Test[module";  ErrorId="ErrorFilteringNamesForUnsupportedWildcards"}
+    $testCases = @{Name="*";                        ErrorId="NameContainsWildcard"},
+                 @{Name="Test_local_m*";            ErrorId="NameContainsWildcard"},
+                 @{Name="Test?local","Test[local";  ErrorId="ErrorFilteringNamesForUnsupportedWildcards"}
 
     It "Should not install resource with wildcard in name" -TestCases $testCases {
         param($Name, $ErrorId)
-        Install-PSResource -Name $Name -Repository $GithubPackagesRepoName -Credential $credential -ErrorVariable err -ErrorAction SilentlyContinue
+        Install-PSResource -Name $Name -Repository $ACRRepoName -ErrorVariable err -ErrorAction SilentlyContinue
         $err.Count | Should -BeGreaterThan 0
         $err[0].FullyQualifiedErrorId | Should -BeExactly "$ErrorId,Microsoft.PowerShell.PSResourceGet.Cmdlets.InstallPSResource"
         $res = Get-InstalledPSResource $testModuleName
@@ -44,58 +40,60 @@ Describe 'Test Install-PSResource for GitHub packages' -tags 'CI' {
     }
 
     It "Install specific module resource by name" {
-        Install-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository
+        Install-PSResource -Name $testModuleName -Repository $ACRRepoName -TrustRepository
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.Version | Should -Be "5.0.0"
     }
 
+    <# TODO:  enable after implementing script functionality
     It "Install specific script resource by name" {
-        Install-PSResource -Name $testScriptName -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository
+        Install-PSResource -Name $testScriptName -Repository $ACRRepoName -TrustRepository
         $pkg = Get-InstalledPSResource $testScriptName
         $pkg.Name | Should -Be $testScriptName
-        $pkg.Version | Should -Be "3.5.0"
+        $pkg.Version | Should -Be "1.0.0"
     }
+    #>
 
     It "Install multiple resources by name" {
         $pkgNames = @($testModuleName, $testModuleName2)
-        Install-PSResource -Name $pkgNames -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository  
+        Install-PSResource -Name $pkgNames -Repository $ACRRepoName -TrustRepository  
         $pkg = Get-InstalledPSResource $pkgNames
         $pkg.Name | Should -Be $pkgNames
     }
 
     It "Should not install resource given nonexistant name" {
-        Install-PSResource -Name "NonExistantModule" -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository -ErrorVariable err -ErrorAction SilentlyContinue
+        Install-PSResource -Name "NonExistantModule" -Repository $ACRRepoName -TrustRepository -ErrorVariable err -ErrorAction SilentlyContinue
         $pkg = Get-InstalledPSResource "NonExistantModule"
         $pkg | Should -BeNullOrEmpty
         $err.Count | Should -BeGreaterThan 0
-        $err[0].FullyQualifiedErrorId | Should -BeExactly "InstallPackageFailure,Microsoft.PowerShell.PSResourceGet.Cmdlets.InstallPSResource" 
+        $err[0].FullyQualifiedErrorId | Should -BeExactly "ResourceNotFound,Microsoft.PowerShell.PSResourceGet.Cmdlets.InstallPSResource" 
     }
 
     # Do some version testing, but Find-PSResource should be doing thorough testing
     It "Should install resource given name and exact version" {
-        Install-PSResource -Name $testModuleName -Version "1.0.0" -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository
+        Install-PSResource -Name $testModuleName -Version "1.0.0" -Repository $ACRRepoName -TrustRepository
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.Version | Should -Be "1.0.0"
     }
 
     It "Should install resource given name and exact version with bracket syntax" {
-        Install-PSResource -Name $testModuleName -Version "[1.0.0]" -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository  
+        Install-PSResource -Name $testModuleName -Version "[1.0.0]" -Repository $ACRRepoName -TrustRepository  
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.Version | Should -Be "1.0.0"
     }
 
     It "Should install resource given name and exact range inclusive [1.0.0, 5.0.0]" {
-        Install-PSResource -Name $testModuleName -Version "[1.0.0, 5.0.0]" -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository  
+        Install-PSResource -Name $testModuleName -Version "[1.0.0, 5.0.0]" -Repository $ACRRepoName -TrustRepository  
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.Version | Should -Be "5.0.0"
     }
 
     It "Should install resource given name and exact range exclusive (1.0.0, 5.0.0)" {
-        Install-PSResource -Name $testModuleName -Version "(1.0.0, 5.0.0)" -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository  
+        Install-PSResource -Name $testModuleName -Version "(1.0.0, 5.0.0)" -Repository $ACRRepoName -TrustRepository  
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.Version | Should -Be "3.0.0"
@@ -104,52 +102,50 @@ Describe 'Test Install-PSResource for GitHub packages' -tags 'CI' {
     # TODO: Update this test and others like it that use try/catch blocks instead of Should -Throw
     It "Should not install resource with incorrectly formatted version such as exclusive version (1.0.0.0)" {
         $Version = "(1.0.0.0)"
-        try {
-            Install-PSResource -Name $testModuleName -Version $Version -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository -ErrorAction SilentlyContinue
-        }
-        catch
-        {}
-        $Error[0].FullyQualifiedErrorId | Should -be "IncorrectVersionFormat,Microsoft.PowerShell.PSResourceGet.Cmdlets.InstallPSResource"
+        { Install-PSResource -Name $testModuleName -Version $Version -Repository $ACRRepoName -TrustRepository -ErrorAction SilentlyContinue } | Should -Throw -ErrorId "IncorrectVersionFormat,Microsoft.PowerShell.PSResourceGet.Cmdlets.InstallPSResource"
 
         $res = Get-InstalledPSResource $testModuleName
         $res | Should -BeNullOrEmpty
     }
 
     It "Install resource when given Name, Version '*', should install the latest version" {
-        Install-PSResource -Name $testModuleName -Version "*" -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository
+        Install-PSResource -Name $testModuleName -Version "*" -Repository $ACRRepoName -TrustRepository
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.Version | Should -Be "5.0.0"
     }
 
+    <# TODO: enable when prerelease functionality is implemented
     It "Install resource with latest (including prerelease) version given Prerelease parameter" {
-        Install-PSResource -Name $testModuleName -Prerelease -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository 
+        Install-PSResource -Name $testModuleName -Prerelease -Repository $ACRRepoName -TrustRepository 
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.Version | Should -Be "5.2.5"
         $pkg.Prerelease | Should -Be "alpha001"
     }
+    #>
 
     It "Install resource via InputObject by piping from Find-PSresource" {
-        Find-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential | Install-PSResource -Credential $credential -TrustRepository
+        Find-PSResource -Name $testModuleName -Repository $ACRRepoName | Install-PSResource -TrustRepository 
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.Version | Should -Be "5.0.0"
     }
 
-    It "Install resource with companyname and repository source location and validate properties" {
-        Install-PSResource -Name $testModuleName -Version "5.2.5-alpha001" -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository
-        $pkg = Get-InstalledPSResource $testModuleName
-        $pkg.Version | Should -Be "5.2.5"
-        $pkg.Prerelease | Should -Be "alpha001"
-
-        $pkg.CompanyName | Should -Be "Anam Navied"
-        $pkg.RepositorySourceLocation | Should -Be $GithubPackagesRepoUri
+    It "Install resource with copyright, description and repository source location and validate properties" {
+        $testModule = "test_module"
+        Install-PSResource -Name $testModule -Version "7.0.0" -Repository $ACRRepoName -TrustRepository
+        $pkg = Get-InstalledPSResource $testModule
+        $pkg.Name | Should -Be $testModule
+        $pkg.Version | Should -Be "7.0.0"
+        $pkg.Copyright | Should -Be "(c) Anam Navied. All rights reserved."
+        $pkg.Description | Should -Be "This is a test module, for PSGallery team internal testing. Do not take a dependency on this package. This version contains tags for the package."
+        $pkg.RepositorySourceLocation | Should -Be $ACRRepoUri
     }
 
     # Windows only
     It "Install resource under CurrentUser scope - Windows only" -Skip:(!(Get-IsWindows)) {
-        Install-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository -Scope CurrentUser
+        Install-PSResource -Name $testModuleName -Repository $ACRRepoName -TrustRepository -Scope CurrentUser
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.InstalledLocation.ToString().Contains("Documents") | Should -Be $true
@@ -157,8 +153,7 @@ Describe 'Test Install-PSResource for GitHub packages' -tags 'CI' {
 
     # Windows only
     It "Install resource under AllUsers scope - Windows only" -Skip:(!((Get-IsWindows) -and (Test-IsAdmin))) {
-        Uninstall-PSResource -Name $testModuleName -SkipDependencyCheck -Scope AllUsers
-        Install-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository -Scope AllUsers -Verbose
+        Install-PSResource -Name $testModuleName -Repository $ACRRepoName -TrustRepository -Scope AllUsers -Verbose
         $pkg = Get-InstalledPSResource $testModuleName -Scope AllUsers
         $pkg.Name | Should -Be $testModuleName
         $pkg.InstalledLocation.ToString().Contains("Program Files") | Should -Be $true
@@ -166,7 +161,7 @@ Describe 'Test Install-PSResource for GitHub packages' -tags 'CI' {
 
     # Windows only
     It "Install resource under no specified scope - Windows only" -Skip:(!(Get-IsWindows)) {
-        Install-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository
+        Install-PSResource -Name $testModuleName -Repository $ACRRepoName -TrustRepository
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.InstalledLocation.ToString().Contains("Documents") | Should -Be $true
@@ -175,7 +170,7 @@ Describe 'Test Install-PSResource for GitHub packages' -tags 'CI' {
     # Unix only
     # Expected path should be similar to: '/home/janelane/.local/share/powershell/Modules'
     It "Install resource under CurrentUser scope - Unix only" -Skip:(Get-IsWindows) {
-        Install-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository -Scope CurrentUser
+        Install-PSResource -Name $testModuleName -Repository $ACRRepoName -TrustRepository -Scope CurrentUser
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.InstalledLocation.ToString().Contains("$env:HOME/.local") | Should -Be $true
@@ -184,40 +179,40 @@ Describe 'Test Install-PSResource for GitHub packages' -tags 'CI' {
     # Unix only
     # Expected path should be similar to: '/home/janelane/.local/share/powershell/Modules'
     It "Install resource under no specified scope - Unix only" -Skip:(Get-IsWindows) {
-        Install-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository
+        Install-PSResource -Name $testModuleName -Repository $ACRRepoName -TrustRepository
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.InstalledLocation.ToString().Contains("$env:HOME/.local") | Should -Be $true
     }
 
     It "Should not install resource that is already installed" {
-        Install-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository
+        Install-PSResource -Name $testModuleName -Repository $ACRRepoName -TrustRepository
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
-        Install-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository -WarningVariable WarningVar -warningaction SilentlyContinue
+        Install-PSResource -Name $testModuleName -Repository $ACRRepoName -TrustRepository -WarningVariable WarningVar -warningaction SilentlyContinue
         $WarningVar | Should -Not -BeNullOrEmpty
     }
 
     It "Reinstall resource that is already installed with -Reinstall parameter" {
-        Install-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential -TrustRepository
+        Install-PSResource -Name $testModuleName -Repository $ACRRepoName -TrustRepository
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.Version | Should -Be "5.0.0"
-        Install-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential -Reinstall -TrustRepository
+        Install-PSResource -Name $testModuleName -Repository $ACRRepoName -Reinstall -TrustRepository
         $pkg = Get-InstalledPSResource $testModuleName
         $pkg.Name | Should -Be $testModuleName
         $pkg.Version | Should -Be "5.0.0"
     }
 
     It "Install PSResourceInfo object piped in" {
-        Find-PSResource -Name $testModuleName -Version "1.0.0.0" -Repository $GithubPackagesRepoName -Credential $credential | Install-PSResource -Credential $credential -TrustRepository
+        Find-PSResource -Name $testModuleName -Version "1.0.0.0" -Repository $ACRRepoName | Install-PSResource -TrustRepository
         $res = Get-InstalledPSResource -Name $testModuleName
         $res.Name | Should -Be $testModuleName
         $res.Version | Should -Be "1.0.0"
     }
 
     It "Install module using -PassThru" {
-        $res = Install-PSResource -Name $testModuleName -Repository $GithubPackagesRepoName -Credential $credential -PassThru -TrustRepository
+        $res = Install-PSResource -Name $testModuleName -Repository $ACRRepoName -PassThru -TrustRepository
         $res.Name | Should -Contain $testModuleName
     }
 }
