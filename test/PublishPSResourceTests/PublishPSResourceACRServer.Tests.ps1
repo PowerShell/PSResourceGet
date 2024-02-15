15eda3764c611d7b94019338d648975b94f07d96
alerickson <25858831+alerickson@users.noreply.github.com>
2024-02-15 13:25:39 -0800

    Add Should Not Throw

--- test/PublishPSResourceTests/PublishPSResourceACRServer.Tests.ps1
+++ test/PublishPSResourceTests/PublishPSResourceACRServer.Tests.ps1
@@ -222,7 +222,7 @@ Describe "Test Publish-PSResource" -tags 'CI' {
         New-ModuleManifest -Path (Join-Path -Path $script:PublishModuleBase -ChildPath "$script:PublishModuleName.psd1") -ModuleVersion $version -Description "$script:PublishModuleName module"
         New-Item -Path (Join-Path -Path $script:PublishModuleBase -ChildPath $testFile) -Force
 
-        Publish-PSResource -Path $script:PublishModuleBase -Repository $ACRRepoName
+        { Publish-PSResource -Path $script:PublishModuleBase -Repository $ACRRepoName -ErrorAction Stop } | Should -Not -Throw
 
         Save-PSResource -Name $script:PublishModuleName -Repository $ACRRepoName -AsNupkg -Path $TestDrive 
         # Must change .nupkg to .zip so that Expand-Archive can work on Windows PowerShell
