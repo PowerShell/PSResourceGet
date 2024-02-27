8e0e7399e62e76167b54d6c5abca591e896e1a09
Anam Navied <anam.naviyou@gmail.com>
2024-02-27 16:18:04 -0500

    add tests for Find for scripts

--- test/FindPSResourceTests/FindPSResourceACRServer.Tests.ps1
+++ test/FindPSResourceTests/FindPSResourceACRServer.Tests.ps1
@@ -8,6 +8,7 @@ Describe 'Test HTTP Find-PSResource for ACR Server Protocol' -tags 'CI' {
 
     BeforeAll{
         $testModuleName = "test_local_mod"
+        $testScript = "testscript"
         $ACRRepoName = "ACRRepo"
         $ACRRepoUri = "https://psresourcegettest.azurecr.io"
         Get-NewPSResourceRepositoryFile
@@ -136,5 +137,21 @@ Describe 'Test HTTP Find-PSResource for ACR Server Protocol' -tags 'CI' {
         $res | Should -BeNullOrEmpty
         $err.Count | Should -BeGreaterThan 0
         $err[0].FullyQualifiedErrorId | Should -BeExactly "FindAllFailure,Microsoft.PowerShell.PSResourceGet.Cmdlets.FindPSResource"
+    }
+
+    It "Should find script given Name" {
+        # FindName()
+        $res = Find-PSResource -Name $testScript -Repository $ACRRepoName
+        $res | Should -Not -BeNullOrEmpty
+        $res.Name | Should -Be $testScript
+        $res.Type | Should -Be "Script"
+    }
+
+    It "Should find script given Name and Version" {
+        # FindVersion()
+        $res = Find-PSResource -Name $testScript -Version "1.2.3.4"
+        $res | Should -Not -BeNullOrEmpty
+        $res.Name | Should -Be $testScript
+        $res.Type | Should -Be "Script"
     }
 }
