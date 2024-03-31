68cc6eba8bde3da4a017ed1fe4d55ef438d58792
Olav Rønnestad Birkeland <o-l-a-v@users.noreply.github.com>
2024-03-31 13:43:25 +0200

    Added manual check for new test

--- test/SavePSResourceTests/SavePSResourceV2Tests.ps1
+++ test/SavePSResourceTests/SavePSResourceV2Tests.ps1
@@ -175,6 +175,9 @@ Describe 'Test HTTP Save-PSResource for V2 Server Protocol' -tags 'CI' {
 
     It "Save script without using -IncludeXML" {
         Save-PSResource -Name $testScriptName -Repository $PSGalleryName -Path $SaveDir -TrustRepository | Should -Not -Throw
+
+        $SavedScriptFile = [System.IO.Path]::Combine($SaveDir,('{0}.ps1' -f $testScriptName))
+        [System.IO.File]::Exists($SavedScriptFile) | Should -BeTrue
     }
 
     It "Save script using -IncludeXML" {
