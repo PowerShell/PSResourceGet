b23ff42c7d6c919fa36442cbeab109d33f4f7c3b
Sean Williams <72675818+sean-r-williams@users.noreply.github.com>
2024-04-23 22:10:03 -0700

    Initial implementation of builder classes for NuGet v2 filters/query strings

---
+++ src/code/V2QueryBuilder.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration.Assemblies;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
+{
+    internal class NuGetV2QueryBuilder
+    {
+
+        internal Dictionary<string, string> AdditionalParameters { get; private set; }
+
+        internal NuGetV2FilterBuilder FilterBuilder { get; private set; }
+
+        internal bool ShouldEmitEmptyFilter = false;
+
+        internal NuGetV2QueryBuilder()
+        {
+
+            FilterBuilder = new NuGetV2FilterBuilder();
+            AdditionalParameters = new Dictionary<string, string> { };
+        }
+
+        internal NuGetV2QueryBuilder(Dictionary<string, string> parameters) : this()
+        {
+            AdditionalParameters = new Dictionary<string, string>(parameters);
+        }
+        internal string BuildQueryString()
+        {
+
+            var QueryParameters = HttpUtility.ParseQueryString("");
+
+
+            if (FilterBuilder.CriteriaCount > 0 || ShouldEmitEmptyFilter)
+            {
+                QueryParameters["$filter"] = FilterBuilder.BuildFilterString();
+            }
+
+            foreach (var parameter in AdditionalParameters)
+            {
+                QueryParameters[parameter.Key] = parameter.Value;
+            }
+
+            return QueryParameters.ToString();
+
+        }
+
+    }
+
+    internal class NuGetV2FilterBuilder
+    {
+
+        internal NuGetV2FilterBuilder()
+        {
+
+        }
+
+        private HashSet<String> FilterCriteria = new HashSet<String>();
+
+        /// <summary>
+        ///     Convert the builder's provided set of filter criteria into an OData-compatible <c>filter</c> string.
+        /// </summary>
+        /// <remarks>
+        ///     Criteria order is not guaranteed.
+        /// 
+        ///     Filter criteria are individually parenthesized, then combined with the <c>and</c> operator.
+        /// </remarks>
+        /// <returns>
+        ///     Filter criteria combined into a single string.
+        /// </returns>
+        /// <example>
+        ///     The following example will emit one of the two values:
+        ///     <list type="bullet">
+        ///         <item>
+        ///             <description><c>(IsPrerelease eq false) and (Id eq 'Microsoft.PowerShell.PSResourceGet')</c></description>
+        ///         </item>
+        ///         <item>
+        ///             <description><c>(Id eq 'Microsoft.PowerShell.PSResourceGet') and (IsPrerelease eq false)</c></description>
+        ///         </item>
+        ///     </list>
+        /// </example>
+        public string BuildFilterString()
+        {
+
+            if (FilterCriteria.Count == 0)
+            {
+                return "";
+            }
+
+            // Figure out the expected size of our filter string, based on:
+            int ExpectedSize = FilterCriteria.Select(x => x.Length).Sum() // The length of the filter criteria themselves.
+                + 7 * (FilterCriteria.Count - 1) // The length of the combining string, ") and (", interpolated between the filters.
+                + 2; // The two outer parentheses.
+
+            // Allocate a StringBuilder with our calculated capacity. 
+            // This helps right-size memory allocation and reduces performance impact from resizing the builder's internal capacity.
+            StringBuilder sb = new StringBuilder(ExpectedSize);
+
+            // StringBuilder.AppendJoin() is not available in .NET 4.8.1/.NET Standard 2,
+            // so we have to make do with repeated calls to Append().
+
+            sb.Append("(");
+
+            int CriteriaAdded = 0;
+
+            foreach (string filter in FilterCriteria)
+            {
+                sb.Append(filter);
+                CriteriaAdded++;
+                if (CriteriaAdded < FilterCriteria.Count)
+                {
+                    sb.Append(") and (");
+                }
+                else
+                {
+                    sb.Append(")");
+                }
+            }
+
+            return sb.ToString();
+
+        }
+
+        public bool AddCriteria(string criteria)
+        {
+            if (string.IsNullOrEmpty(criteria))
+            {
+                throw new ArgumentException("Criteria cannot be null or empty.", nameof(criteria));
+            }
+            else
+            {
+                return FilterCriteria.Add(criteria);
+            }
+        }
+
+        public bool RemoveCriteria(string criteria) => FilterCriteria.Remove(criteria);
+
+        public int CriteriaCount => FilterCriteria.Count;
+
+    }
+}
