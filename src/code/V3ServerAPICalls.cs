510527eafce383178e53f0c6cb91474921a63e98
alerickson <25858831+alerickson@users.noreply.github.com>
2024-07-10 14:22:55 -0700

    Remove extra initialization of _sessionClient

--- src/code/V3ServerAPICalls.cs
+++ src/code/V3ServerAPICalls.cs
@@ -75,7 +75,6 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                 _sessionClient = new HttpClient(handler);
             };
 
-            _sessionClient = new HttpClient(handler);
             _sessionClient.Timeout = TimeSpan.FromMinutes(10);
             _sessionClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", userAgentString);
 
