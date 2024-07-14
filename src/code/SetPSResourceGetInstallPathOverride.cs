802184e202d1bdb06cf6771074596c39b01b13a7
Olav Rønnestad Birkeland <6450056+o-l-a-v@users.noreply.github.com>
2024-07-14 12:04:32 +0200

    Added new cmdlet

---
+++ src/code/SetPSResourceGetInstallPathOverride.cs
@@ -0,0 +1,70 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.PowerShell.PSResourceGet.UtilClasses;
+using System;
+using System.Management.Automation;
+
+namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
+{
+    /// <summary>
+    /// The Set-PSResourceGetInstallPathOverride cmdlet is used to override install path for PS resources.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Set,"PSResourceGetInstallPathOverride",SupportsShouldProcess = true)]
+    [Alias("Update-PSResourceGetInstallPathOverride")]
+    [OutputType(typeof(void))]
+
+    public sealed class SetPSResourceGetInstallPathOverride : PSCmdlet
+    {
+        #region Parameters
+
+        /// <summary>
+        /// Specifies the desired path for the override.
+        /// </summary>
+        [Parameter(Position = 0, ValueFromPipeline = true, Mandatory = true)]
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Specifies the scope of installation.
+        /// </summary>
+        [Parameter(Position = 1)]
+        public ScopeType Scope { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        protected override void BeginProcessing()
+        {
+            // Validate path is not null or empty
+            if (string.IsNullOrEmpty(Path)) {
+                ThrowTerminatingError(
+                    new ErrorRecord(
+                        new PSInvalidOperationException($"Error input path is null or empty: '{Path}'"),
+                        "PathMissingExpectedSubdirectories",
+                        ErrorCategory.InvalidOperation,
+                        this
+                    )
+                );
+            }
+
+            // Validate path is absolute
+            if (!System.IO.Path.IsPathRooted(Path)) {
+                ThrowTerminatingError(
+                    new ErrorRecord(
+                        new PSInvalidOperationException($"Error input path is not rooted / absolute: '{Path}'"),
+                        "PathMissingExpectedSubdirectories",
+                        ErrorCategory.InvalidOperation,
+                        this
+                    )
+                );
+            }
+        }
+
+        protected override void ProcessRecord()
+        {
+        }
+
+        #endregion
+    }
+}
