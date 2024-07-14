69863bb866188bfaf62e5d2dc791925de3cedffe
Olav Rønnestad Birkeland <6450056+o-l-a-v@users.noreply.github.com>
2024-07-14 18:45:41 +0200

    Finished barebone functionality

--- src/code/SetPSResourceGetInstallPathOverride.cs
+++ src/code/SetPSResourceGetInstallPathOverride.cs
@@ -3,6 +3,7 @@
 
 using Microsoft.PowerShell.PSResourceGet.UtilClasses;
 using System;
+using System.Collections.Specialized;
 using System.Management.Automation;
 using System.Runtime.InteropServices;
 
@@ -42,7 +43,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                 ThrowTerminatingError(
                     new ErrorRecord(
                         new PSInvalidOperationException($"Error this only works on Windows for now"),
-                        "PathMissingExpectedSubdirectories",
+                        "OsIsNotWindows",
                         ErrorCategory.InvalidOperation,
                         this
                     )
@@ -54,7 +55,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                 ThrowTerminatingError(
                     new ErrorRecord(
                         new PSInvalidOperationException($"Error input path is null or empty: '{Path}'"),
-                        "PathMissingExpectedSubdirectories",
+                        "InputPathIsEmpty",
                         ErrorCategory.InvalidArgument,
                         this
                     )
@@ -66,7 +67,23 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                 ThrowTerminatingError(
                     new ErrorRecord(
                         new PSInvalidOperationException($"Error input path is not rooted / absolute: '{Path}'"),
-                        "PathMissingExpectedSubdirectories",
+                        "InputPathIsNotRooted",
+                        ErrorCategory.InvalidArgument,
+                        this
+                    )
+                );
+            }
+
+            // Validate path can be expanded
+            try {
+                Environment.ExpandEnvironmentVariables(Path);
+            }
+            catch (Exception)
+            {
+                ThrowTerminatingError(
+                    new ErrorRecord(
+                        new PSInvalidOperationException($"Error input path could not be expanded: '{Path}'"),
+                        "InputPathCannotBeExpanded",
                         ErrorCategory.InvalidArgument,
                         this
                     )
@@ -95,7 +112,46 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                 "PSModulePath",
                 envScope
             );
+            if (String.IsNullOrEmpty(PSModulePath)) {
+                WriteVerbose(String.Format("PSModulePath in {0} context is empty.", envScope.ToString()));
+                System.Environment.SetEnvironmentVariable(
+                    "PSModulePath",
+                    Path,
+                    envScope
+                );
+            }
             WriteVerbose(string.Format("Current value of PSModulePath in {0} context: '{1}'", envScope.ToString(), PSModulePath));
+            StringCollection PSModulePaths = new StringCollection();
+            foreach (string Item in PSModulePath.Trim(';').Split(';')) {
+                try {
+                    PSModulePaths.Add(System.Environment.ExpandEnvironmentVariables(Item));
+                }
+                catch {
+                    WriteVerbose(string.Format("Will not validate '{0}' as it could not be expanded.", Item));
+                }
+            }
+            if (PSModulePaths.Contains(System.Environment.ExpandEnvironmentVariables(Path))) {
+                WriteVerbose(String.Format("Override install path is already in PSModulePath for scope '{0}'", envScope.ToString()));
+            }
+            else {
+                WriteVerbose(
+                    String.Format(
+                        "Override install path is not already in PSModulePath for scope '{0}'",
+                        envScope.ToString()
+                    )
+                );
+                System.Environment.SetEnvironmentVariable(
+                    "PSModulePath",
+                    String.Format("{0};{1}", Path, PSModulePath),
+                    envScope
+                );
+                WriteVerbose(
+                    String.Format(
+                        "Override install path was successfully added to PSModulePath for scope '{0}'.",
+                        envScope.ToString()
+                    )
+                );
+            }
         }
 
         #endregion
