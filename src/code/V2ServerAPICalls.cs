c7969145ac5ed62cee72fc5dc1f5c2cbde74cacc
Anam Navied <anam.naviyou@gmail.com>
2025-01-07 14:01:50 -0500

    Modify filter query parameter (#1761)

--- src/code/V2ServerAPICalls.cs
+++ src/code/V2ServerAPICalls.cs
@@ -355,7 +355,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                 filterBuilder.AddCriterion($"Id eq '{packageName}'");
             }
 
-            filterBuilder.AddCriterion(includePrerelease ? "IsAbsoluteLatestVersion" : "IsLatestVersion");
+            filterBuilder.AddCriterion(includePrerelease ? "IsAbsoluteLatestVersion eq true" : "IsLatestVersion eq true");
             if (type != ResourceType.None) {
                 filterBuilder.AddCriterion(GetTypeFilterForRequest(type));
             }
@@ -424,7 +424,7 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                 filterBuilder.AddCriterion($"Id eq '{packageName}'");
             }
 
-            filterBuilder.AddCriterion(includePrerelease ? "IsAbsoluteLatestVersion" : "IsLatestVersion");
+            filterBuilder.AddCriterion(includePrerelease ? "IsAbsoluteLatestVersion eq true" : "IsLatestVersion eq true");
             if (type != ResourceType.None) {
                 filterBuilder.AddCriterion(GetTypeFilterForRequest(type));
             }
@@ -915,14 +915,26 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
             // JFrog/Artifactory requires an empty search term to enumerate all packages in the feed
             if (_isJFrogRepo) {
                 queryBuilder.SearchTerm = "''";
+
+                if (includePrerelease) {
+                    queryBuilder.AdditionalParameters["includePrerelease"] = "true";
+                    // note: we add 'eq true' because some PMPs (currently we know of JFrog, but others may do this too) will proxy the query unedited to the upstream remote and if that's PSGallery, it doesn't handle IsAbsoluteLatestVersion correctly
+                    filterBuilder.AddCriterion("IsAbsoluteLatestVersion eq true");
+                } else {
+                    // note: we add 'eq true' because some PMPs (currently we know of JFrog, but others may do this too) will proxy the query unedited to the upstream remote and if that's PSGallery, it doesn't handle IsLatestVersion correctly
+                    filterBuilder.AddCriterion("IsLatestVersion eq true");
+                }
+            }
+            else {
+                // For ADO, 'IsLatestVersion eq true' and 'IsAbsoluteLatestVersion eq true' in the filter create a bad request error, so we use 'IsLatestVersion' or 'IsAbsoluteLatestVersion' only
+                if (includePrerelease) {
+                    queryBuilder.AdditionalParameters["includePrerelease"] = "true";
+                    filterBuilder.AddCriterion("IsAbsoluteLatestVersion");
+                } else {
+                    filterBuilder.AddCriterion("IsLatestVersion");
+                }
             }
 
-            if (includePrerelease) {
-                queryBuilder.AdditionalParameters["includePrerelease"] = "true";
-                filterBuilder.AddCriterion("IsAbsoluteLatestVersion");
-            } else {
-                filterBuilder.AddCriterion("IsLatestVersion");
-            }
             var requestUrlV2 = $"{Repository.Uri}{typeEndpoint}/Search()?{queryBuilder.BuildQueryString()}";
             return HttpRequestCall(requestUrlV2, out errRecord);
         }
@@ -952,16 +964,24 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                 queryBuilder.AdditionalParameters["$orderby"] = "Id desc";
             }
 
-            // JFrog/Artifactory requires an empty search term to enumerate all packages in the feed
-            if (_isJFrogRepo) {
-                queryBuilder.SearchTerm = "''";
-            }
-
             if (includePrerelease) {
                 queryBuilder.AdditionalParameters["includePrerelease"] = "true";
-                filterBuilder.AddCriterion("IsAbsoluteLatestVersion");
+                if (_isJFrogRepo) {
+                    // note: we add 'eq true' because some PMPs (currently we know of JFrog, but others may do this too) will proxy the query unedited to the upstream remote and if that's PSGallery, it doesn't handle IsAbsoluteLatestVersion correctly
+                    filterBuilder.AddCriterion("IsAbsoluteLatestVersion eq true");
+                }
+                else {
+                    // For ADO, 'IsLatestVersion eq true' and 'IsAbsoluteLatestVersion eq true' in the filter create a bad request error, so we use 'IsLatestVersion' or 'IsAbsoluteLatestVersion' only
+                    filterBuilder.AddCriterion("IsAbsoluteLatestVersion");
+                }
             } else {
-                filterBuilder.AddCriterion("IsLatestVersion");
+                if (_isJFrogRepo) {
+                    filterBuilder.AddCriterion("IsLatestVersion eq true");
+                }
+                else {
+                    // For ADO, 'IsLatestVersion eq true' and 'IsAbsoluteLatestVersion eq true' in the filter create a bad request error, so we use 'IsLatestVersion' or 'IsAbsoluteLatestVersion' only
+                    filterBuilder.AddCriterion("IsLatestVersion");
+                }
             }
 
             filterBuilder.AddCriterion($"substringof('PS{(isSearchingModule ? "Module" : "Script")}', Tags) eq true");
@@ -996,11 +1016,24 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
 
             if (includePrerelease) {
                 queryBuilder.AdditionalParameters["includePrerelease"] = "true";
-                filterBuilder.AddCriterion("IsAbsoluteLatestVersion");
+                if (_isJFrogRepo) {
+                    // note: we add 'eq true' because some PMPs (currently we know of JFrog, but others may do this too) will proxy the query unedited to the upstream remote and if that's PSGallery, it doesn't handle IsAbsoluteLatestVersion correctly
+                    filterBuilder.AddCriterion("IsAbsoluteLatestVersion eq true");
+                }
+                else {
+                    // For ADO, 'IsLatestVersion eq true' and 'IsAbsoluteLatestVersion eq true' in the filter create a bad request error, so we use 'IsLatestVersion' or 'IsAbsoluteLatestVersion' only
+                    filterBuilder.AddCriterion("IsAbsoluteLatestVersion");
+                }
             } else {
-                filterBuilder.AddCriterion("IsLatestVersion");
+                if (_isJFrogRepo) {
+                    // note: we add 'eq true' because some PMPs (currently we know of JFrog, but others may do this too) will proxy the query unedited to the upstream remote and if that's PSGallery, it doesn't handle IsLatestVersion correctly
+                    filterBuilder.AddCriterion("IsLatestVersion eq true");
+                }
+                else {
+                    // For ADO, 'IsLatestVersion eq true' and 'IsAbsoluteLatestVersion eq true' in the filter create a bad request error, so we use 'IsLatestVersion' or 'IsAbsoluteLatestVersion' only
+                    filterBuilder.AddCriterion("IsLatestVersion");
+                }
             }
-
 
             // can only find from Modules endpoint
             var tagPrefix = isSearchingForCommands ? "PSCommand_" : "PSDscResource_";
@@ -1038,11 +1071,24 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
 
             if (includePrerelease) {
                 queryBuilder.AdditionalParameters["includePrerelease"] = "true";
-                filterBuilder.AddCriterion("IsAbsoluteLatestVersion");
+                if (_isJFrogRepo) {
+                    // note: we add 'eq true' because some PMPs (currently we know of JFrog, but others may do this too) will proxy the query unedited to the upstream remote and if that's PSGallery, it doesn't handle IsAbsoluteLatestVersion correctly
+                    filterBuilder.AddCriterion("IsAbsoluteLatestVersion eq true");
+                }
+                else {
+                    // For ADO, 'IsLatestVersion eq true' and 'IsAbsoluteLatestVersion eq true' in the filter create a bad request error, so we use 'IsLatestVersion' or 'IsAbsoluteLatestVersion' only
+                    filterBuilder.AddCriterion("IsAbsoluteLatestVersion");
+                }
             } else {
-                filterBuilder.AddCriterion("IsLatestVersion");
+                if (_isJFrogRepo) {
+                    // note: we add 'eq true' because some PMPs (currently we know of JFrog, but others may do this too) will proxy the query unedited to the upstream remote and if that's PSGallery, it doesn't handle IsLatestVersion correctly
+                    filterBuilder.AddCriterion("IsLatestVersion eq true");
+                }
+                else {
+                    // For ADO, 'IsLatestVersion eq true' and 'IsAbsoluteLatestVersion eq true' in the filter create a bad request error, so we use 'IsLatestVersion' or 'IsAbsoluteLatestVersion' only
+                    filterBuilder.AddCriterion("IsLatestVersion");
+                }
             }
-
 
             var names = packageName.Split(new char[] {'*'}, StringSplitOptions.RemoveEmptyEntries);
 
@@ -1131,13 +1177,27 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
                 queryBuilder.AdditionalParameters["$orderby"] = "Id desc";
             }
 
+            // JFrog/Artifactory requires an empty search term to enumerate all packages in the feed
             if (includePrerelease) {
                 queryBuilder.AdditionalParameters["includePrerelease"] = "true";
-                filterBuilder.AddCriterion("IsAbsoluteLatestVersion");
+                if (_isJFrogRepo) {
+                    // note: we add 'eq true' because some PMPs (currently we know of JFrog, but others may do this too) will proxy the query unedited to the upstream remote and if that's PSGallery, it doesn't handle IsAbsoluteLatestVersion correctly
+                    filterBuilder.AddCriterion("IsAbsoluteLatestVersion eq true");
+                }
+                else {
+                    // For ADO, 'IsLatestVersion eq true' and 'IsAbsoluteLatestVersion eq true' in the filter create a bad request error, so we use 'IsLatestVersion' or 'IsAbsoluteLatestVersion' only
+                    filterBuilder.AddCriterion("IsAbsoluteLatestVersion");
+                }
             } else {
-                filterBuilder.AddCriterion("IsLatestVersion");
+                if (_isJFrogRepo) {
+                    // note: we add 'eq true' because some PMPs (currently we know of JFrog, but others may do this too) will proxy the query unedited to the upstream remote and if that's PSGallery, it doesn't handle IsLatestVersion correctly
+                    filterBuilder.AddCriterion("IsLatestVersion eq true");
+                }
+                else {
+                    // For ADO, 'IsLatestVersion eq true' and 'IsAbsoluteLatestVersion eq true' in the filter create a bad request error, so we use 'IsLatestVersion' or 'IsAbsoluteLatestVersion' only
+                    filterBuilder.AddCriterion("IsLatestVersion");
+                }
             }
-
 
             var names = packageName.Split(new char[] {'*'}, StringSplitOptions.RemoveEmptyEntries);
 
