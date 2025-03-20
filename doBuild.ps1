9ae3f769d5aedc9e893a873dd495a3c4128144df
Aditya Patwardhan <adityap@microsoft.com>
2025-03-20 12:26:24 -0700

    Add explicit dependency of Pkcs

--- doBuild.ps1
+++ doBuild.ps1
@@ -108,8 +108,6 @@ function DoBuild
                 'NuGet.Protocol'
                 'NuGet.Versioning'
                 'System.Buffers'
-                'System.ClientModel'
-                'System.Collections.Immutable'
                 'System.Diagnostics.DiagnosticSource'
                 'System.IO.FileSystem.AccessControl'
                 'System.Memory.Data'
