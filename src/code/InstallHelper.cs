d7a53fc73116d6c04219d1e992b7e63d613c7b9f
Olav Rønnestad Birkeland <6450056+o-l-a-v@users.noreply.github.com>
2025-08-11 11:48:12 +0200

    Experimenting on getting case of GitHub Packages packages

--- src/code/InstallHelper.cs
+++ src/code/InstallHelper.cs
@@ -943,8 +943,8 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
 
                 File.Delete(pathToFile);
 
-                var moduleManifest = Path.Combine(tempDirNameVersion, pkgName + PSDataFileExt);
-                var scriptPath = Path.Combine(tempDirNameVersion, pkgName + PSScriptFileExt);
+                string moduleManifest = Path.Combine(tempDirNameVersion, pkgName + PSDataFileExt);
+                string scriptPath = Path.Combine(tempDirNameVersion, pkgName + PSScriptFileExt);
 
                 bool isModule = File.Exists(moduleManifest);
                 bool isScript = File.Exists(scriptPath);
@@ -979,6 +979,15 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
 
                         return false;
                     }
+
+                    // Get module actual name with correct casing
+                    string moduleManifestActualFileName = Path.GetFileName(
+                        Directory.GetFiles(
+                            tempDirNameVersion,
+                            $"{pkgName}.psd1"
+                        )[0]
+                    );
+                    _cmdletPassedIn.WriteVerbose($"pkgName: \"{pkgName}\", actual name of manifest: \"${moduleManifestActualFileName}");
 
                     if (!Utils.TryReadManifestFile(
                         manifestFilePath: moduleManifest,
@@ -1032,6 +1041,15 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
 
                         return false;
                     }
+
+                    // Get script actual name with correct casing
+                    string scriptActualFileName = Path.GetFileNameWithoutExtension(
+                        Directory.GetFiles(
+                            tempDirNameVersion,
+                            $"{pkgName}.ps1"
+                        )[0]
+                    );
+                    _cmdletPassedIn.WriteVerbose($"pkgName: \"{pkgName}\", actual name of script: \"{scriptActualFileName}\"");
                 }
                 else
                 {
@@ -1040,6 +1058,15 @@ namespace Microsoft.PowerShell.PSResourceGet.Cmdlets
 
                     _cmdletPassedIn.WriteVerbose($"This resource is not a PowerShell package and will be installed to the modules path: {installPath}.");
                     isModule = true;
+
+                    // Get actual name from .nuspec file
+                    string resourceNuspecFileName = Path.GetFileNameWithoutExtension(
+                        Directory.GetFiles(
+                            tempDirNameVersion,
+                            $"{pkgName}.nuspec"
+                        )[0]
+                    );
+                    _cmdletPassedIn.WriteVerbose($"pkgName: \"{pkgName}\", actual name of nuspec file: \"{resourceNuspecFileName}\"");
                 }
 
                 installPath = _savePkg ? _pathsToInstallPkg.First() : installPath;
