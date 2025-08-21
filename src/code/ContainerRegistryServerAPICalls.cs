64dc19bfa1de7f50eb7d82dd30fa5a9827fab801
Aditya Patwardhan <adityap@microsoft.com>
2025-08-21 09:02:09 -0700

    Might revert

--- src/code/ContainerRegistryServerAPICalls.cs
+++ src/code/ContainerRegistryServerAPICalls.cs
@@ -1001,6 +1001,8 @@ namespace Microsoft.PowerShell.PSResourceGet
                     _cmdletPassedIn);
             }
 
+            _cmdletPassedIn.WriteVerbose("Exiting ContainerRegistryServerAPICalls::GetHttpResponseJObjectUsingDefaultHeaders() with NULL");
+
             return null;
         }
 
