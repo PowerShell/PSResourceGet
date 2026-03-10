72233eee7a1fffb2db7748df6236f352cf9ed907
alerickson <25858831+alerickson@users.noreply.github.com>
2026-03-09 17:20:35 -0700

    add logging to test

--- test/SavePSResourceTests/SavePSResourceV3.Tests.ps1
+++ test/SavePSResourceTests/SavePSResourceV3.Tests.ps1
@@ -156,7 +156,8 @@ Describe 'Test HTTP Save-PSResource for V3 Server Protocol' -tags 'CI' {
     }
 
     It "Save module and its dependencies" {
-        $res = Save-PSResource 'TestModuleWithDependencyE' -Repository $NuGetGalleryName -TrustRepository -PassThru
+        $DebugPreference = 'SilentlyContinue'
+        $res = Save-PSResource 'TestModuleWithDependencyE' -Repository $NuGetGalleryName -TrustRepository -PassThru -Debug -Verbose
         $res.Length | Should -Be 4
     }
 
