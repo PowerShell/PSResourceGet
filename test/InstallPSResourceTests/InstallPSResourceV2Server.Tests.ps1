a8419e8f8f7529651fb832d5104f52a98a28cd9e
alerickson <25858831+alerickson@users.noreply.github.com>
2026-03-10 20:44:49 -0700

    add verbose, debug and write to authenticode test

--- test/InstallPSResourceTests/InstallPSResourceV2Server.Tests.ps1
+++ test/InstallPSResourceTests/InstallPSResourceV2Server.Tests.ps1
@@ -569,8 +569,10 @@ Describe 'Test Install-PSResource for V2 Server scenarios' -tags 'CI' {
 
     # Test that AuthenticodeCheck parameter displays warning on non-Windows
     It "Install with AuthenticodeCheck on non-Windows should display warning" -Skip:(Get-IsWindows) {
-        Install-PSResource -Name $testModuleName -Repository $PSGalleryName -TrustRepository -AuthenticodeCheck -WarningVariable warn -WarningAction SilentlyContinue
+        Install-PSResource -Name $testModuleName -Repository $PSGalleryName -TrustRepository -AuthenticodeCheck -WarningVariable warn -WarningAction SilentlyContinue -Verbose -Debug
+        Write-Host $warn[0] -ForegroundColor Green
         $warn[0] | Should -Match "Authenticode check cannot be performed on Linux or MacOS"
+        
         $res = Get-InstalledPSResource $testModuleName
         $res.Name | Should -Be $testModuleName
     }
